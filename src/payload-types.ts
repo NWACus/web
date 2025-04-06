9503370853a1e5767518fefe954a344f9faf5941
rchlfryn <rachelafryan@gmail.com>
2025-04-06 16:33:31 -0700

    Make sizes for columns dynamic

--- src/payload-types.ts
+++ src/payload-types.ts
@@ -820,7 +820,6 @@ export interface Form {
 export interface IconList {
   columns?:
     | {
-        size?: ('oneThird' | 'half' | 'twoThirds' | 'full') | null;
         icon: number | Media;
         title: string;
         richText?: {
@@ -1440,7 +1439,6 @@ export interface IconListSelect<T extends boolean = true> {
   columns?:
     | T
     | {
-        size?: T;
         icon?: T;
         title?: T;
         richText?: T;
