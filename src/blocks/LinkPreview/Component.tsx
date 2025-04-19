fd831539b7f54bcb13ccb4a98fd64178ae063a93
rchlfryn <rachelafryan@gmail.com>
2025-04-19 12:10:52 -0700

    Update styling

--- src/blocks/LinkPreview/Component.tsx
+++ src/blocks/LinkPreview/Component.tsx
@@ -16,27 +16,30 @@ export const LinkPreviewBlock = (props: LinkPreviewBlockProps) => {
   const colsSpanClass = colsClasses[numOfCols]
 
   return (
-    <div className="bg-slate-500 py-16">
-      <div className="container grid grid-cols-4 lg:grid-cols-12 gap-x-12">
+    <div className="bg-slate-500 py-8">
+      <div className="container grid grid-cols-4 lg:grid-cols-12 gap-x-2">
         {cards &&
           cards.length > 0 &&
-          cards.map((col, index) => {
-            const { image, link, text, title } = col
+          cards.map((card, index) => {
+            const { image, link, text, title } = card
             const lastOddElement = numOfCols % 2 && index === numOfCols - 1
-
             return (
               <div
                 className={cn(
-                  `col-span-4 md:col-span-2 ${colsSpanClass} ${lastOddElement && 'md:col-span-full'} p-6 bg-white rounded-xl`,
+                  `col-span-4 sm:col-span-2 my-1 ${colsSpanClass} 
+                  ${lastOddElement && 'sm:col-start-2'} 
+                  p-6 bg-white rounded-lg grid`,
                 )}
                 key={index}
               >
                 <Media imgClassName={'w-full border border-border'} resource={image} />
-
-                <h3 className="text-lg font-bold mt-6">{title}</h3>
-                <p className="my-4">{text}</p>
-
-                <CMSLink {...link} />
+                <div className="flex flex-col justify-between items-start">
+                  <div>
+                    <h3 className="text-lg font-semibold mt-6">{title}</h3>
+                    <p className="text-sm mt-2 mb-6">{text}</p>
+                  </div>
+                  <CMSLink {...link} />
+                </div>
               </div>
             )
           })}
