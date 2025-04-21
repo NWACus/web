0dc2467cdb7c672c2ef8725bc355266107fce12e
Steve Kuznetsov <stekuznetsov@microsoft.com>
2025-04-21 07:24:06 -0600

    *: add biographies, teams
    Two new collections are added:
    
     - biographies: Information about someone associated with an avalanche
       center. These are disjoint from Users (but may refer to them) since:
       - not all people we want to highlight have logins, some are emeritus
       - some users are highlighted on more then one center's page
     - teams: A list of biographies with a title.
    
    These collections are threaded through to the front-end using custom
    Blocks, and a "who we are" page is built using these blocks.
    
    Signed-off-by: Steve Kuznetsov <stekuznetsov@microsoft.com>

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -1,6 +1,6 @@
 import type { CollectionConfig } from 'payload'
 
-import { accessByTenant } from '@/access/byTenant'
+import { accessByTenantWithPermissiveRead } from '@/access/byTenant'
 import { filterByTenant } from '@/access/filterByTenant'
 import { tenantField } from '@/fields/tenantField'
 import {
@@ -21,11 +21,11 @@ type BeforeOperationHook = Exclude<
 >[number]
 const prefixFilename: BeforeOperationHook = async ({ req }) => {
   if (req.file) {
-    req.payload.logger.info(`media: have data ${JSON.stringify(req.data)}`)
+    req.payload.logger.debug(`media: have data ${JSON.stringify(req.data)}`)
     const media = req.data
     let tenantSlug: string | undefined = undefined
     if (media && 'tenant' in media && typeof media.tenant === 'number') {
-      req.payload.logger.info(`media: fetching slug for tenant ${media.tenant}`)
+      req.payload.logger.debug(`media: fetching slug for tenant ${media.tenant}`)
       const tenant = await req.payload.find({
         collection: 'tenants',
         overrideAccess: true,
@@ -38,42 +38,31 @@ const prefixFilename: BeforeOperationHook = async ({ req }) => {
           },
         },
       })
-      req.payload.logger.info(
+      req.payload.logger.debug(
         `media: got ${tenant.docs ? tenant.docs.length : 0} documents querying for tenant ${media.tenant}`,
       )
       if (tenant.docs && tenant.docs.length > 0) {
         tenantSlug = tenant.docs[0].slug
-        req.payload.logger.info(`media: using result slug for tenant ${tenant.docs[0].slug}`)
+        req.payload.logger.debug(`media: using result slug for tenant ${tenant.docs[0].slug}`)
       }
     } else if (media && 'tenant' in media && typeof media.tenant === 'object') {
-      req.payload.logger.info(`media: using literal slug for tenant ${media.tenant.slug}`)
+      req.payload.logger.debug(`media: using literal slug for tenant ${media.tenant.slug}`)
       tenantSlug = media.tenant.slug
     } else {
-      req.payload.logger.info(
+      req.payload.logger.debug(
         `media: unknown tenant, have media ${!!media}, tenant in media: ${media && 'tenant' in media}, type of tenant ${media && 'tenant' in media ? typeof media.tenant : 'unknown'}, tenant: ${media && 'tenant' in media ? JSON.stringify(media.tenant) : 'unknown'}`,
       )
     }
     if (tenantSlug) {
       req.file.name = `${tenantSlug}-` + req.file.name
-      req.payload.logger.info(`updated media filename to ${req.file.name}`)
+      req.payload.logger.debug(`updated media filename to ${req.file.name}`)
     }
   }
 }
 
-const {
-  create: createAccess,
-  update: updateAccess,
-  delete: deleteAccess,
-} = accessByTenant('media') || {}
-
 export const Media: CollectionConfig = {
   slug: 'media',
-  access: {
-    create: createAccess,
-    read: () => true, // TODO: seems to be necessary for frontend, probably ok? maybe we just stop using unauthenticated clients or enforcing auth for front-end at all, but will it break live editor?
-    update: updateAccess,
-    delete: deleteAccess,
-  },
+  access: accessByTenantWithPermissiveRead('media'),
   admin: {
     baseListFilter: filterByTenant,
     group: 'Content',
