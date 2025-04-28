239605304c887cdbde327488b303141ecc9573db
rchlfryn <rachelafryan@gmail.com>
2025-04-27 18:50:45 -0700

    Update rending for non column content

--- src/blocks/Content/Component.tsx
+++ src/blocks/Content/Component.tsx
@@ -4,22 +4,35 @@ import { cn } from '@/utilities/ui'
 import type { ContentBlock as ContentBlockProps } from '@/payload-types'
 
 export const ContentBlock = (props: ContentBlockProps) => {
-  const { columns } = props
+  const { enableColumns, columns, content } = props
+  const numOfCols = columns?.length ?? 1
+
+  const colsClasses: { [key: number]: string } = {
+    1: 'lg:col-span-12',
+    2: 'lg:col-span-6',
+    3: 'lg:col-span-4',
+    4: 'lg:col-span-3',
+  }
+  const colsSpanClass = colsClasses[numOfCols]
 
   return (
     <div className="container my-16">
       <div className="grid grid-cols-4 lg:grid-cols-12 gap-y-8 gap-x-16">
-        {columns &&
-          columns.length > 0 &&
-          columns.map((col, index) => {
+        {enableColumns ? (
+          columns?.map((col, index) => {
             const { richText } = col
 
             return (
-              <div className={cn(`col-span-4 md:col-span-2 lg:col-span-full`)} key={index}>
+              <div className={cn(`col-span-4 md:col-span-2 ${colsSpanClass}`)} key={index}>
                 {richText && <RichText data={richText} enableGutter={false} />}
               </div>
             )
-          })}
+          })
+        ) : (
+          <div className={cn(`col-span-4 lg:col-span-12`)}>
+            {content?.richText && <RichText data={content.richText} enableGutter={false} />}
+          </div>
+        )}
       </div>
     </div>
   )
