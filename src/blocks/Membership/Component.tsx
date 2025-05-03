de7f79f4866826585e6cda630a119a690b55a921
rchlfryn <rachelafryan@gmail.com>
2025-05-03 11:23:39 -0700

    Update styling universally for p tag, and Membership block

--- src/blocks/Membership/Component.tsx
+++ src/blocks/Membership/Component.tsx
@@ -5,12 +5,7 @@ import RichText from '@/components/RichText'
 export const MembershipBlock = ({ richText }: MembershipBlockProps) => {
   return (
     <div className="container">
-      <div className="bg-card rounded border-border border p-4 flex flex-col gap-8 md:flex-row md:justify-between md:items-center">
-        <div className="max-w-[48rem] flex items-center">
-          {richText && <RichText className="mb-0" data={richText} enableGutter={false} />}
-        </div>
-        <div className="flex flex-col gap-8"></div>
-      </div>
+      {richText && <RichText className="mb-0" data={richText} enableGutter={false} />}
     </div>
   )
 }
