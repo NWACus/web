6e21947d516fb6e0234b59e28e1550eaa4585161
Kellen Busby <kellenbusby@gmail.com>
2025-05-07 15:48:17 -0700

    removing temporary debug logging + removing vercel domain add (not needed) + removing protocol from SERVER_DOMAIN env var

--- src/utilities/getURL.ts
+++ src/utilities/getURL.ts
@@ -2,16 +2,9 @@ import canUseDOM from './canUseDOM'
 
 export const getServerSideURL = () => {
   const domain = process.env.SERVER_DOMAIN || process.env.VERCEL_URL
-  const url = domain
+  return domain
     ? `https://${domain}`
     : process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000'
-
-  console.log('Determined url: ', url)
-  console.log('process.env.SERVER_DOMAIN: ', process.env.SERVER_DOMAIN)
-  console.log('process.env.VERCEL_URL: ', process.env.VERCEL_URL)
-  console.log('process.env.NEXT_PUBLIC_SERVER_URL: ', process.env.NEXT_PUBLIC_SERVER_URL)
-
-  return url
 }
 
 export const getClientSideURL = () => {
@@ -20,16 +13,8 @@ export const getClientSideURL = () => {
     const domain = window.location.hostname
     const port = window.location.port
 
-    const url = `${protocol}//${domain}${port ? `:${port}` : ''}`
-
-    console.log('Returning url from getClientSideURL: ', url)
-
-    return url
+    return `${protocol}//${domain}${port ? `:${port}` : ''}`
   }
 
-  const url = process.env.NEXT_PUBLIC_SERVER_URL || ''
-
-  console.log('Returning url from getClientSideURL: ', url)
-
-  return url
+  return process.env.NEXT_PUBLIC_SERVER_URL || ''
 }
