1c32c65adf6f35337d212131989d594789076bdd
rchlfryn <rachelafryan@gmail.com>
2025-05-09 14:43:43 -0700

    Add callout to membership block

--- src/blocks/Membership/Component.tsx
+++ src/blocks/Membership/Component.tsx
@@ -2,10 +2,21 @@ import type { MembershipBlock as MembershipBlockProps } from '@/payload-types'
 
 import RichText from '@/components/RichText'
 
-export const MembershipBlock = ({ richText }: MembershipBlockProps) => {
+export const MembershipBlock = ({ callout, enableCallout, richText }: MembershipBlockProps) => {
   return (
     <div className="container">
-      {richText && <RichText className="mb-0" data={richText} enableGutter={false} />}
+      {enableCallout ? (
+        <div className="flex flex-col md:flex-row items-center gap-4">
+          {richText && <RichText className="mb-0" data={richText} enableGutter={false} />}
+          {callout && (
+            <div className="p-6 bg-slate-200 rounded-sm">
+              <RichText className="mb-0" data={callout} enableGutter={false} />
+            </div>
+          )}
+        </div>
+      ) : (
+        <>{richText && <RichText className="mb-0" data={richText} enableGutter={false} />}</>
+      )}
     </div>
   )
 }
