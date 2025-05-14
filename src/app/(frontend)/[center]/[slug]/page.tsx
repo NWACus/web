d35e92bc08c2a865d9a4b9955a9b416894b5a2d5
Kellen Busby <kellenbusby@gmail.com>
2025-05-14 12:04:08 -0700

    removing the concept of a 'home' route

--- src/app/(frontend)/[center]/[slug]/page.tsx
+++ src/app/(frontend)/[center]/[slug]/page.tsx
@@ -48,13 +48,13 @@ type Args = {
 
 type PathArgs = {
   center: string
-  slug?: string
+  slug: string
 }
 
 export default async function Page({ params: paramsPromise }: Args) {
   const payload = await getPayload({ config: configPromise })
   const { isEnabled: draft } = await draftMode()
-  const { center, slug = 'home' } = await paramsPromise
+  const { center, slug } = await paramsPromise
   const url = '/' + center + '/' + slug
 
   const page: PageType | null = await queryPageBySlug({
@@ -86,7 +86,7 @@ export async function generateMetadata({
 }: {
   params: Promise<PathArgs>
 }): Promise<Metadata> {
-  const { center, slug = 'home' } = await paramsPromise
+  const { center, slug } = await paramsPromise
   const page = await queryPageBySlug({
     center: center,
     slug: slug,
