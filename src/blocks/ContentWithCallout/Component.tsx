c0e569280308f85fd4887923cec6ef1e02f30138
rchlfryn <rachelafryan@gmail.com>
2025-05-15 09:22:45 -0700

    Update buttons and colors

--- src/blocks/ContentWithCallout/Component.tsx
+++ src/blocks/ContentWithCallout/Component.tsx
@@ -13,7 +13,7 @@ export const ContentWithCalloutBlock = ({
         <div className="flex flex-col md:flex-row items-center gap-4">
           {richText && <RichText className="mb-0" data={richText} enableGutter={false} />}
           {callout && (
-            <div className="p-6 bg-slate-200 rounded-sm">
+            <div className="p-6 bg-brand-200 rounded-sm">
               <RichText className="mb-0" data={callout} enableGutter={false} />
             </div>
           )}
