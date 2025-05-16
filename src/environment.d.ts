0ba22419d365a8ff2bb75343fe9dd6e56da660eb
Kellen Busby <kellenbusby@gmail.com>
2025-05-16 00:12:02 -0700

    updating GOOGLE_MAPS_API_KEY to NEXT_PUBLIC_GOOGLE_MAPS_API_KEY

--- src/environment.d.ts
+++ src/environment.d.ts
@@ -5,7 +5,9 @@ declare global {
       DATABASE_URI: string
       NEXT_PUBLIC_SERVER_URL: string
       VERCEL_PROJECT_PRODUCTION_URL: string
-      GOOGLE_MAPS_API_KEY: string
+      NEXT_PUBLIC_GOOGLE_MAPS_API_KEY: string
+      NEXT_PUBLIC_NAC_WIDGETS_BUCKET: string
+      NEXT_PUBLIC_NAC_WIDGETS_VERSION: string
     }
   }
 }
