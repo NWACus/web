1d7344ff491838edc3928763279c6b567b952e2f
rchlfryn <rachelafryan@gmail.com>
2025-05-29 12:07:42 -0400

    Update copyright to be current year

--- src/Footer/Component.tsx
+++ src/Footer/Component.tsx
@@ -30,6 +30,8 @@ export async function Footer({ center }: { center?: string }) {
     typeof privacy === 'object',
     `Depth not set correctly when querying footer. Privacy for tenant ${center} not an object.`,
   )
+
+  const currentYear = new Date().getFullYear()
   return (
     <footer className="mt-auto border-t border-border bg-footer text-footer-foreground">
       <div className="container py-8 gap-8 grid grid-cols-3">
@@ -66,7 +68,9 @@ export async function Footer({ center }: { center?: string }) {
         </div>
       </div>
       <div className="container text-center pb-8">
-        <p className="mb-2">All Content © 2017 – 2025 {name}</p>
+        <p className="mb-2">
+          All Content © {currentYear} {name}
+        </p>
         <div className="flex gap-x-2 justify-center">
           {terms && (
             <a href={`/${terms.slug}`} className="underline">
