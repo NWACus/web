bcdc3ee6a241df6db0fde30606897745bfa2f070
Kellen Busby <kellenbusby@gmail.com>
2025-06-04 10:16:14 -0700

    updating default widget version as this is the minimum version that supports the controlled mounting changes

--- src/utilities/getNACWidgetsConfig.ts
+++ src/utilities/getNACWidgetsConfig.ts
@@ -3,7 +3,7 @@ import { getCachedGlobal } from './getGlobals'
 export async function getNACWidgetsConfig() {
   const nacWidgetsConfig = await getCachedGlobal('nacWidgetsConfig', 1)()
 
-  const version = nacWidgetsConfig?.version || '20250313'
+  const version = nacWidgetsConfig?.version || '20250602'
   const baseUrl = nacWidgetsConfig?.baseUrl || 'https://du6amfiq9m9h7.cloudfront.net/public/v2'
 
   return { version, baseUrl }
