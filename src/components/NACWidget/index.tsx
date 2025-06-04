4fdbab919858ae4c24306ea2107e6602600a1d88
Kellen Busby <kellenbusby@gmail.com>
2025-06-04 10:08:54 -0700

    making comment explaining the need for a parent container clearer

--- src/components/NACWidget/index.tsx
+++ src/components/NACWidget/index.tsx
@@ -139,7 +139,7 @@ export function NACWidget({
 
   return (
     <>
-      {/* container used for css selectors */}
+      {/* container used for css selectors in src/app/(frontend)/[center]/nac-widgets.css */}
       <div id="widget-container" data-widget={widget}>
         <div id={`nac-widget-${widget}`} />
       </div>
