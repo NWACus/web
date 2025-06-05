2ad2cbdca444cd06fe26474819dab02c7de496aa
rchlfryn <rachelafryan@gmail.com>
2025-06-05 10:18:28 -0400

    Remove grid classes for now

--- src/components/CollectionArchive/index.tsx
+++ src/components/CollectionArchive/index.tsx
@@ -15,8 +15,6 @@ export const CollectionArchive = (props: Props) => {
   return (
     <div className={cn('container')}>
       <div>
-        {/* Grid classes */}
-        {/* <div className="grid grid-cols-4 sm:grid-cols-8 lg:grid-cols-12 gap-y-4 gap-x-4 lg:gap-y-8 lg:gap-x-8 xl:gap-x-8"> */}
         <div>
           {posts?.map((result, index) => {
             if (typeof result === 'object' && result !== null) {
