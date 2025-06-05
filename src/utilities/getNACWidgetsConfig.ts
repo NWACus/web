5564da46a4552c955e5a24b7d0d79064eada7449
Kellen Busby <kellenbusby@gmail.com>
2025-06-05 09:30:30 -0700

    throwing if nacWidgetsConfig version or baseUrl is falsy instead of using default values

--- src/utilities/getNACWidgetsConfig.ts
+++ src/utilities/getNACWidgetsConfig.ts
@@ -1,10 +1,20 @@
+import invariant from 'tiny-invariant'
 import { getCachedGlobal } from './getGlobals'
 
 export async function getNACWidgetsConfig() {
   const nacWidgetsConfig = await getCachedGlobal('nacWidgetsConfig', 1)()
 
-  const version = nacWidgetsConfig?.version || '20250602'
-  const baseUrl = nacWidgetsConfig?.baseUrl || 'https://du6amfiq9m9h7.cloudfront.net/public/v2'
+  const version = nacWidgetsConfig?.version
+  invariant(
+    version,
+    'Could not determine NAC widgets version. Ensure this is set in the NAC Widgets Config global.',
+  )
+
+  const baseUrl = nacWidgetsConfig?.baseUrl
+  invariant(
+    baseUrl,
+    'Could not determine NAC widgets base url. Ensure this is set in the NAC Widgets Config global.',
+  )
 
   return { version, baseUrl }
 }
