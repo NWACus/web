de48139242745d9956c778e65e6655412bd15fbc
Kellen Busby <kellenbusby@gmail.com>
2025-06-06 13:43:43 -0700

    adding missing var to deps array

--- src/app/(frontend)/[center]/observations/submit/NACContainerRemover.client.tsx
+++ src/app/(frontend)/[center]/observations/submit/NACContainerRemover.client.tsx
@@ -3,36 +3,39 @@
 import { useEffect } from 'react'
 
 export function NACContainerRemover({ containerElementId }: { containerElementId: string }) {
-  useEffect(function removeNACContainerClassName() {
-    const removeContainerClassName = () => {
-      const containerElement = document.getElementById(`${containerElementId}`)
+  useEffect(
+    function removeNACContainerClassName() {
+      const removeContainerClassName = () => {
+        const containerElement = document.getElementById(`${containerElementId}`)
 
-      if (containerElement && containerElement.classList.contains('nac-container')) {
-        containerElement.classList.remove('nac-container')
-      }
-    }
-
-    const observer = new MutationObserver((mutations) => {
-      for (const mutation of mutations) {
-        if (mutation.type === 'childList' && document.querySelector(`#${containerElementId}`)) {
-          removeContainerClassName()
-          observer.disconnect()
+        if (containerElement && containerElement.classList.contains('nac-container')) {
+          containerElement.classList.remove('nac-container')
         }
       }
-    })
 
-    const container = document.querySelector('#widget-container')
-    if (container) {
-      observer.observe(container, {
-        childList: true,
-        subtree: true,
+      const observer = new MutationObserver((mutations) => {
+        for (const mutation of mutations) {
+          if (mutation.type === 'childList' && document.querySelector(`#${containerElementId}`)) {
+            removeContainerClassName()
+            observer.disconnect()
+          }
+        }
       })
-    }
 
-    return () => {
-      observer.disconnect()
-    }
-  }, [])
+      const container = document.querySelector('#widget-container')
+      if (container) {
+        observer.observe(container, {
+          childList: true,
+          subtree: true,
+        })
+      }
+
+      return () => {
+        observer.disconnect()
+      }
+    },
+    [containerElementId],
+  )
 
   return null
 }
