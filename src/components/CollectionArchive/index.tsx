709072b4bb40de063e015fad5eebfdb354fcd0af
rchlfryn <rachelafryan@gmail.com>
2025-06-17 22:00:20 -0400

    Clean up styling for main blog page (still meh on tablet)

--- src/components/CollectionArchive/index.tsx
+++ src/components/CollectionArchive/index.tsx
@@ -11,20 +11,18 @@ export const CollectionArchive = (props: Props) => {
   const { posts } = props
 
   return (
-    <div>
-      <div>
-        {posts?.map((result, index) => {
-          if (typeof result === 'object' && result !== null) {
-            return (
-              <div className="col-span-4" key={index}>
-                <PostPreviewHorizontal className="h-full" doc={result} relationTo="posts" />
-              </div>
-            )
-          }
+    <>
+      {posts?.map((result, index) => {
+        if (typeof result === 'object' && result !== null) {
+          return (
+            <div className="py-6" key={index}>
+              <PostPreviewHorizontal className="h-full" doc={result} relationTo="posts" />
+            </div>
+          )
+        }
 
-          return null
-        })}
-      </div>
-    </div>
+        return null
+      })}
+    </>
   )
 }
