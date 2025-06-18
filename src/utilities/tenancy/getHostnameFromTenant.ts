997c1b92586336c14fb0e00d740b08c0623d0bfa
Kellen Busby <kellenbusby@gmail.com>
2025-06-17 21:40:53 -0700

    adding more debug logging

--- src/utilities/tenancy/getHostnameFromTenant.ts
+++ src/utilities/tenancy/getHostnameFromTenant.ts
@@ -3,6 +3,9 @@ import { ROOT_DOMAIN } from '../domain'
 import { getProductionTenantSlugs } from './getProductionTenants'
 
 export function getHostnameFromTenant(tenant: Tenant | null) {
+  console.log('tenant in getHostnameFromTenant: ', JSON.stringify(tenant))
+  console.log('ROOT_DOMAIN in getHostnameFromTenant: ', ROOT_DOMAIN)
+
   if (!tenant) return ROOT_DOMAIN
 
   if (getProductionTenantSlugs().includes(tenant.slug)) {
