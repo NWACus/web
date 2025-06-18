3a5fc73e24a2ea92ae75f2b5873717360de757bf
Kellen Busby <kellenbusby@gmail.com>
2025-06-17 22:25:41 -0700

    adding logging step in preview workflow temporarily + cleaning up the .next directory after seeding

--- seed.sh
+++ seed.sh
@@ -16,3 +16,6 @@ setup_dev_server
 echo "[INFO] Creating the bootstrap user and seeding the database..."
 curl -s -H 'Content-Type: application/json' -H 'Accept: application/json' -X POST --fail-with-body http://localhost:3000/api/users/first-register --data '{"email":"bootstrap@avy.com","password":"test","confirm-password":"test","name":"Bootstrap User"}' > login.json
 curl -H "Authorization: Bearer $( jq --raw-output .token <login.json )" -X POST --fail-with-body http://localhost:3000/next/seed
+
+echo "[INFO] Cleaning up .next directory..."
+rm -rf .next
