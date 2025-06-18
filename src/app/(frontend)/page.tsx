4109507a5a53f1c1f40be5b85502b49845120f30
Kellen Busby <kellenbusby@gmail.com>
2025-06-17 21:05:19 -0700

    adding some temporary debug logging

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -24,16 +24,23 @@ export default async function LandingPage() {
             <Link href="/admin">Login</Link>
           </div>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-            {tenants.docs.map(async (tenant) => (
-              <Link
-                key={tenant.slug}
-                href={getURL(getHostnameFromTenant(tenant))}
-                className="p-6 border rounded-lg hover:bg-gray-100 dark:hover:bg-gray-800 transition"
-              >
-                <h2 className="mb-2">{tenant.name}</h2>
-                <p className="no-underline">Visit site ➡️</p>
-              </Link>
-            ))}
+            {tenants.docs.map(async (tenant) => {
+              const hostname = getHostnameFromTenant(tenant)
+              const href = getURL(hostname)
+
+              console.log(`href for tenant ${tenant.slug}: ${href}`)
+              console.log(`hostname for tenant ${tenant.slug}: ${hostname}`)
+              return (
+                <Link
+                  key={tenant.slug}
+                  href={href}
+                  className="p-6 border rounded-lg hover:bg-gray-100 dark:hover:bg-gray-800 transition"
+                >
+                  <h2 className="mb-2">{tenant.name}</h2>
+                  <p className="no-underline">Visit site ➡️</p>
+                </Link>
+              )
+            })}
           </div>
         </div>
       </div>
