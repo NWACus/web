71aefa7a66b82656190388750ce2bba79cf5414a
rchlfryn <rachelafryan@gmail.com>
2025-06-27 12:34:02 -0700

    Rewrite PostsGrid as a server component - WIP but sort is working

--- src/app/(frontend)/[center]/posts/page.tsx
+++ src/app/(frontend)/[center]/posts/page.tsx
@@ -1,40 +1,74 @@
 import type { Metadata } from 'next/types'
 
+import { CollectionArchive } from '@/components/CollectionArchive'
+import { PageRange } from '@/components/PageRange'
+import { Pagination } from '@/components/Pagination'
 import configPromise from '@payload-config'
 import { getPayload } from 'payload'
-import { PostsGrid } from './posts-grid'
-
-export const dynamic = 'force-static'
-export const revalidate = 600
+import { PostsSort } from './posts-sort'
 
 type Args = {
-  params: Promise<PathArgs>
+  params: Promise<{
+    center: string[]
+  }>
+  searchParams: Promise<{ [key: string]: string }>
 }
 
-type PathArgs = {
-  center: string
-}
-
-export default async function Page({ params }: Args) {
-  const payload = await getPayload({ config: configPromise })
+export default async function Page({ params, searchParams }: Args) {
   const { center } = await params
+  const resolvedSearchParams = await searchParams
+  const sort = resolvedSearchParams?.sort || '-publishedAt'
 
-  const tags = await payload.find({
-    collection: 'tags',
-    depth: 1,
-    limit: 99,
-    pagination: false,
+  const payload = await getPayload({ config: configPromise })
+
+  const posts = await payload.find({
+    collection: 'posts',
+    depth: 2,
+    limit: 12,
     where: {
       'tenant.slug': {
         equals: center,
       },
+      _status: {
+        equals: 'published',
+      },
     },
-    sort: 'name',
+    sort,
   })
 
   return (
     <div className="pt-24 pb-24">
-      <PostsGrid tags={tags.docs} center={center} />
+      <div>
+        <div className="container mb-16 flex flex-col-reverse md:flex-row flex-1 gap-6">
+          {/* {error && <div>{`${error.message || ''}`}</div>} */}
+          <div className="grow">
+            {posts && posts?.totalDocs > 0 ? (
+              <CollectionArchive posts={posts.docs} />
+            ) : (
+              <h3>There are no posts matching these results.</h3>
+            )}
+          </div>
+
+          <div className="sm:w-[280px]">
+            {/* Sort */}
+            <PostsSort initialSort={sort} />
+          </div>
+        </div>
+        {posts.totalPages > 1 && posts.page && (
+          <div className="container mb-8">
+            <Pagination page={posts?.page} totalPages={posts?.totalPages} />
+            <PageRange
+              collectionLabels={{
+                plural: 'Posts',
+                singular: 'Post',
+              }}
+              currentPage={posts?.page}
+              limit={12}
+              totalDocs={posts?.totalDocs}
+            />
+          </div>
+        )}
+      </div>
     </div>
   )
 }
---
+++ src/app/(frontend)/[center]/posts/posts-sort.tsx
@@ -0,0 +1,48 @@
+'use client'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
+import { useEffect, useState } from 'react'
+
+type Props = {
+  initialSort: string
+}
+
+export const PostsSort = ({ initialSort }: Props) => {
+  const router = useRouter()
+  const pathname = usePathname()
+  const searchParams = useSearchParams()
+
+  const [sortOption, setSortOption] = useState(initialSort)
+
+  useEffect(() => {
+    setSortOption(initialSort)
+  }, [initialSort])
+
+  useEffect(() => {
+    const params = new URLSearchParams(searchParams.toString())
+    params.set('sort', sortOption)
+    router.push(`${pathname}?${params.toString()}`)
+  }, [pathname, router, searchParams, sortOption])
+
+  return (
+    <div>
+      <h4 className="w-full">Sort</h4>
+      <hr className="p-2" />
+      <Select value={sortOption} onValueChange={setSortOption}>
+        <SelectTrigger className="mb-4">
+          <SelectValue placeholder="Sort" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="-publishedAt">Newest to Oldest</SelectItem>
+          <SelectItem value="publishedAt">Oldest to Newest</SelectItem>
+        </SelectContent>
+      </Select>
+    </div>
+  )
+}
