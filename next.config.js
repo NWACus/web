7bcff331cebd23b965aed869e7056db99bae535d
Kellen Busby <kellenbusby@gmail.com>
2025-07-15 17:58:50 -0700

    attempting to resolve a Sentry error

--- next.config.js
+++ next.config.js
@@ -26,6 +26,9 @@ const nextConfig = {
   },
   reactStrictMode: true,
   redirects,
+  // Resolves an error from Sentry
+  // Reference: https://github.com/getsentry/sentry-javascript/issues/15209#issuecomment-2820494802
+  serverExternalPackages: ['require-in-the-middle'],
   webpack: (config) => {
     // Ignores a nasty-looking but apparently harmless error resulting from importing Sentry in client components
     // Reference: https://github.com/getsentry/sentry-javascript/issues/12077#issuecomment-2407569917
--- sentry-base-config.ts
+++ sentry-base-config.ts
@@ -2,7 +2,7 @@ const sentryBaseConfig = {
   dsn: 'https://7f836bd47bd79570fa140c3b0c60f193@o4504317391929344.ingest.us.sentry.io/4509675069702145',
   tracesSampleRate: 0.5,
   enabled: process.env.NODE_ENV === 'production',
-  environment: process.env.VERCEL_GIT_COMMIT_REF,
+  environment: process.env.VERCEL_GIT_COMMIT_REF || 'local',
 }
 
 export default sentryBaseConfig
