a7ceefa69df3ee253e6c59e52ddf689f4cc5f367
Kellen Busby <kellenbusby@gmail.com>
2025-07-15 18:30:04 -0700

    installing import-in-the-middle

--- next.config.js
+++ next.config.js
@@ -28,7 +28,7 @@ const nextConfig = {
   redirects,
   // Resolves an error from Sentry
   // Reference: https://github.com/getsentry/sentry-javascript/issues/15209#issuecomment-2820494802
-  serverExternalPackages: ['require-in-the-middle'],
+  serverExternalPackages: ['require-in-the-middle', 'import-in-the-middle'],
   webpack: (config) => {
     // Ignores a nasty-looking but apparently harmless error resulting from importing Sentry in client components
     // Reference: https://github.com/getsentry/sentry-javascript/issues/12077#issuecomment-2407569917
