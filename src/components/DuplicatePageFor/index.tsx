7d762184e1bb6b43c76f2d9dc210a6f0f0209c4f
rchlfryn <rachelafryan@gmail.com>
2025-07-15 21:52:20 -0700

    Add permission for multi tenant contributor

--- src/components/DuplicatePageFor/index.tsx
+++ src/components/DuplicatePageFor/index.tsx
@@ -1,8 +1,14 @@
 import { byGlobalRole } from '@/access/byGlobalRole'
+import { byTenantRole } from '@/access/byTenantRole'
+import { EditMenuItemsServerProps } from 'payload'
 import { DuplicatePageForDrawer } from './DuplicatePageForDrawer'
 
-export const DuplicatePageFor = () => {
+export const DuplicatePageFor = ({ user }: EditMenuItemsServerProps) => {
   const isSuperAdmin = byGlobalRole('*', '*')
-  if (!isSuperAdmin) return null
+  const userRolesTenant = user?.roles?.docs?.map((r) => typeof r !== 'number' && r.tenant)
+  const isMultiTenantUser = userRolesTenant ?? 0 > 1
+  const isContributorWithMultiTenants = byTenantRole('*', 'pages') && isMultiTenantUser
+
+  if (!isSuperAdmin && !isContributorWithMultiTenants) return null
   return <DuplicatePageForDrawer />
 }
