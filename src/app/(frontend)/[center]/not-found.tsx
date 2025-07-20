4eda881fc6a4f69eaf04d714843b4275cc8a9778
Kellen Busby <kellenbusby@gmail.com>
2025-07-20 12:41:34 -0700

    hiding breadcrumbs on 404 page

--- src/app/(frontend)/[center]/not-found.tsx
+++ src/app/(frontend)/[center]/not-found.tsx
@@ -1,17 +1,21 @@
 import Link from 'next/link'
 
 import { Button } from '@/components/ui/button'
+import NotFoundClient from './not-found.client'
 
 export default function NotFound() {
   return (
-    <div className="container py-28">
-      <div className="prose max-w-none">
-        <h1 style={{ marginBottom: 0 }}>404</h1>
-        <p className="mb-4">This page could not be found.</p>
+    <>
+      <NotFoundClient />
+      <div className="container py-28">
+        <div className="prose max-w-none">
+          <h1 style={{ marginBottom: 0 }}>404</h1>
+          <p className="mb-4">This page could not be found.</p>
+        </div>
+        <Button asChild>
+          <Link href="/">Go home</Link>
+        </Button>
       </div>
-      <Button asChild variant="default">
-        <Link href="/">Go home</Link>
-      </Button>
-    </div>
+    </>
   )
 }
