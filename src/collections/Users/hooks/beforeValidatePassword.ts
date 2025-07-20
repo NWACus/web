02817de4f9b4dc19a32dc52239aab9aea24774bf
Kellen Busby <kellenbusby@gmail.com>
2025-07-20 14:22:21 -0700

    ignoring password validation for invite operation

--- src/collections/Users/hooks/beforeValidatePassword.ts
+++ src/collections/Users/hooks/beforeValidatePassword.ts
@@ -2,9 +2,19 @@ import { validateStrongPassword } from '@/utilities/passwordValidation'
 import { CollectionBeforeValidateHook } from 'payload'
 
 export const beforeValidatePassword: CollectionBeforeValidateHook = async (args) => {
-  const { data } = args
+  const { data, operation } = args
 
-  if (process.env.NODE_ENV === 'production' && typeof data?.password === 'string') {
+  /**
+   * Since we block access to the built-in create operation in the users collection access control,
+   * the only time operation will be 'create' is during the invite user flow.
+   *
+   * Ignore password validation for invitations because passwords are generated using uuid()
+   */
+  if (
+    process.env.NODE_ENV === 'production' &&
+    typeof data?.password === 'string' &&
+    operation !== 'create'
+  ) {
     const result = validateStrongPassword(data.password)
     if (result !== true) {
       throw new Error(result)
