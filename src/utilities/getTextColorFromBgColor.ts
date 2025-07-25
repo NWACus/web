db3fa233a6c9b3c62e6a3115436b1959ea9deb10
rchlfryn <rachelafryan@gmail.com>
2025-07-24 19:48:50 -0700

    Remove unnecessary color package and write function to return foreground color

--- src/utilities/getTextColorFromBgColor.ts
+++ src/utilities/getTextColorFromBgColor.ts
@@ -1,23 +1,8 @@
-import Color from 'color'
+export default function getTextColorFromBgColor(colorClassName: string): string {
+  const lastDash = colorClassName.lastIndexOf('-')
+  if (lastDash === -1) return colorClassName
 
-import tailwindConfig from 'tailwind.config.mjs'
-import resolveConfig from 'tailwindcss/resolveConfig'
-import { DefaultColors } from 'tailwindcss/types/generated/colors'
-
-type ColorKey = keyof DefaultColors
-type ShadeKey = keyof DefaultColors[ColorKey]
-
-export default function getTextColorFromBgColor(className: string) {
-  const fullConfig = resolveConfig(tailwindConfig)
-
-  const match = className.split('-')
-  const color = match[0] as ColorKey
-  const shade = (match[1] as ShadeKey) || null
-
-  const colorAsHex = shade ? fullConfig.theme.colors[color][shade] : fullConfig.theme.colors[color]
-
-  console.log(colorAsHex)
-
-  const bgColor = Color(colorAsHex)
-  return bgColor.isLight() ? 'text-black' : 'text-white'
+  const color = colorClassName.slice(0, lastDash)
+  const shade = colorClassName.slice(lastDash + 1)
+  return `${color}-foreground-${shade}`
 }
