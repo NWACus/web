fe493c315975c5d83e462111d405e56551e78d97
rchlfryn <rachelafryan@gmail.com>
2025-07-25 15:47:48 -0700

    Add params to confirm the action is unpublish

--- src/hooks/populatePublishedAt.ts
+++ src/hooks/populatePublishedAt.ts
@@ -1,6 +1,11 @@
 import type { CollectionBeforeChangeHook } from 'payload'
 
-export const populatePublishedAt: CollectionBeforeChangeHook = ({ data, operation, req }) => {
+export const populatePublishedAt: CollectionBeforeChangeHook = ({
+  data,
+  operation,
+  originalDoc,
+  req,
+}) => {
   if (operation === 'create' && req.data && !req.data.publishedAt) {
     const now = new Date()
     return {
@@ -14,7 +19,7 @@ export const populatePublishedAt: CollectionBeforeChangeHook = ({ data, operatio
     if (req.data && !req.data.publishedAt) {
       publishedAtDate = new Date()
     }
-    if (req.data && req.data._status === 'draft') {
+    if (req.data && req.data._status === 'draft' && originalDoc._status === 'published') {
       publishedAtDate = null
     }
     return {
