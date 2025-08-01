6f1ec34728d2a27a0a46839829a0406e71111bf9
Kellen Busby <kellenbusby@gmail.com>
2025-08-01 13:57:18 -0700

    revalidating posts and posts sitemap correctly - tenant scoped & if slug changes in addition to unpublished

--- src/collections/Posts/hooks/revalidatePost.ts
+++ src/collections/Posts/hooks/revalidatePost.ts
@@ -4,41 +4,69 @@ import { revalidatePath, revalidateTag } from 'next/cache'
 
 import type { Post } from '@/payload-types'
 
-export const revalidatePost: CollectionAfterChangeHook<Post> = ({
+export const revalidatePost: CollectionAfterChangeHook<Post> = async ({
   doc,
   previousDoc,
   req: { payload, context },
 }) => {
-  // if (!context.disableRevalidate) {
-  //   if (doc._status === 'published') {
-  //     // TODO: revalidate path based on tenant
-  //     const path = `/blog/${doc.slug}`
+  if (!context.disableRevalidate) {
+    let tenant = doc.tenant
 
-  //     payload.logger.info(`Revalidating post at path: ${path}`)
+    if (typeof tenant === 'number') {
+      tenant = await payload.findByID({
+        collection: 'tenants',
+        id: tenant,
+        depth: 2,
+      })
+    }
 
-  //     revalidatePath(path)
-  //     revalidateTag('posts-sitemap')
-  //   }
+    if (doc._status === 'published') {
+      const path = `/${tenant.slug}/blog/${doc.slug}`
 
-  //   // If the post was previously published, we need to revalidate the old path
-  //   if (previousDoc._status === 'published' && doc._status !== 'published') {
-  //     const oldPath = `/blog/${previousDoc.slug}`
+      payload.logger.info(`Revalidating post at path: ${path}`)
 
-  //     payload.logger.info(`Revalidating old post at path: ${oldPath}`)
+      revalidatePath(path)
+      revalidateTag(`posts-sitemap-${tenant.slug}`)
+    }
 
-  //     revalidatePath(oldPath)
-  //     revalidateTag('posts-sitemap')
-  //   }
-  // }
+    // If the post was previously published, and it is no longer published or the slug has changed
+    // we need to revalidate the old path
+    if (
+      previousDoc._status === 'published' &&
+      (doc._status !== 'published' || previousDoc.slug !== doc.slug)
+    ) {
+      const oldPath = `/${tenant.slug}/blog/${previousDoc.slug}`
+
+      payload.logger.info(`Revalidating old post at path: ${oldPath}`)
+
+      revalidatePath(oldPath)
+      revalidateTag(`posts-sitemap-${tenant.slug}`)
+    }
+  }
   return doc
 }
 
-export const revalidateDelete: CollectionAfterDeleteHook<Post> = ({ doc, req: { context } }) => {
+export const revalidateDelete: CollectionAfterDeleteHook<Post> = async ({
+  doc,
+  req: { payload, context },
+}) => {
   if (!context.disableRevalidate) {
-    const path = `/blog/${doc?.slug}`
+    let tenant = doc.tenant
+
+    if (typeof tenant === 'number') {
+      tenant = await payload.findByID({
+        collection: 'tenants',
+        id: tenant,
+        depth: 2,
+      })
+    }
+
+    const path = `/${tenant.slug}/blog/${doc?.slug}`
+
+    payload.logger.info(`Revalidating deleted post at path: ${path}`)
 
     revalidatePath(path)
-    revalidateTag('posts-sitemap')
+    revalidateTag(`posts-sitemap-${tenant.slug}`)
   }
 
   return doc
