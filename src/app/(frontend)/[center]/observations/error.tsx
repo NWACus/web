9c5f6b39be6130f22004c49e2bdc4f5af36304dc
Kellen Busby <kellenbusby@gmail.com>
2025-08-07 14:20:41 -0700

    let's make this even simpler

--- src/app/(frontend)/[center]/observations/error.tsx
+++ src/app/(frontend)/[center]/observations/error.tsx
@@ -2,12 +2,4 @@
 
 import { ErrorBoundary } from '@/components/ErrorBoundary/ErrorBoundary'
 
-export default function Error({
-  error,
-  reset,
-}: {
-  error: Error & { digest?: string }
-  reset: () => void
-}) {
-  return <ErrorBoundary error={error} reset={reset} />
-}
+export default ErrorBoundary
