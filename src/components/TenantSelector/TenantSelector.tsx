86f454ec0712d47fbdeb08d75fd05446a491767d
rchlfryn <rachelafryan@gmail.com>
2025-08-09 10:22:28 -0700

    Use params to change multi tenant behavior

--- src/components/TenantSelector/TenantSelector.tsx
+++ src/components/TenantSelector/TenantSelector.tsx
@@ -1,14 +1,27 @@
 import { isTenantDomainScoped } from '@/utilities/tenancy/isTenantDomainScoped'
+import { type AdminViewServerProps } from 'payload'
 import TenantSelectorClient from './TenantSelector.client'
 
-const TenantSelector = async () => {
+const TenantSelector = async ({ params }: AdminViewServerProps) => {
+  const isGlobalDocument =
+    params?.segments?.includes('settings') || params?.segments?.includes('navigations')
+  const hideTenantSelect =
+    params?.segments?.includes('roles') ||
+    params?.segments?.includes('globalRoles') ||
+    params?.segments?.includes('globals')
   const { isDomainScoped } = await isTenantDomainScoped()
 
   if (isDomainScoped) {
     return null
   }
 
-  return <TenantSelectorClient label="Avalanche Center" />
+  return (
+    <TenantSelectorClient
+      label="Avalanche Center"
+      isGlobalDocument={isGlobalDocument}
+      hideTenantSelect={hideTenantSelect}
+    />
+  )
 }
 
 export default TenantSelector
