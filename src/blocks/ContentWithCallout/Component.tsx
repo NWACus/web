3decc7b8fe3be87efbe6a6a01579c8d3a487e4eb
rchlfryn <rachelafryan@gmail.com>
2025-08-15 13:59:10 -0700

    Fix callout component spacing (#414)

--- src/blocks/ContentWithCallout/Component.tsx
+++ src/blocks/ContentWithCallout/Component.tsx
@@ -6,9 +6,13 @@ export const ContentWithCalloutBlock = ({ callout, richText }: ContentWithCallou
   return (
     <div className="container">
       <div className="flex flex-col md:flex-row items-center gap-4">
-        {richText && <RichText className="mb-0" data={richText} enableGutter={false} />}
+        {richText && (
+          <div className="md:basis-2/3 md:flex-shrink-0 md:flex-grow-0 w-full">
+            <RichText className="mb-0" data={richText} enableGutter={false} />
+          </div>
+        )}
         {callout && (
-          <div className="p-6 bg-brand-200 rounded-sm">
+          <div className="p-6 bg-brand-200 rounded-sm md:basis-1/3 md:flex-shrink-0 md:flex-grow-0 w-full">
             <RichText className="mb-0" data={callout} enableGutter={false} />
           </div>
         )}
