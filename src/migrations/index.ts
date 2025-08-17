c0a3a136cd14f9e0c795edc3b8792a717966ca9d
Kellen Busby <kellenbusby@gmail.com>
2025-08-16 19:19:25 -0700

    migration

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -4,6 +4,7 @@ import * as migration_20250727_005201_add_background_color_to_link_preview from 
 import * as migration_20250807_175342_blocks_in_content_field from './20250807_175342_blocks_in_content_field'
 import * as migration_20250814_191230_diagnostics_global from './20250814_191230_diagnostics_global'
 import * as migration_20250814_223446_update_slug_field from './20250814_223446_update_slug_field'
+import * as migration_20250817_021912_home_page_global_collection from './20250817_021912_home_page_global_collection'
 
 export const migrations = [
   {
@@ -35,5 +36,10 @@ export const migrations = [
     up: migration_20250814_223446_update_slug_field.up,
     down: migration_20250814_223446_update_slug_field.down,
     name: '20250814_223446_update_slug_field',
+  },
+  {
+    up: migration_20250817_021912_home_page_global_collection.up,
+    down: migration_20250817_021912_home_page_global_collection.down,
+    name: '20250817_021912_home_page_global_collection',
   },
 ]
