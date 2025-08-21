e13f2e95be24f8fef8340d2e3506631febed24f8
Kellen Busby <kellenbusby@gmail.com>
2025-08-21 13:23:33 -0700

    changing to rounded-lg

--- src/blocks/BlogList/Component.tsx
+++ src/blocks/BlogList/Component.tsx
@@ -57,7 +57,7 @@ export const BlogListBlockComponent = async (args: BlogListComponentProps) => {
   return (
     <div className={cn(wrapInContainer && bgColorClass)}>
       <div className={cn(wrapInContainer && 'container py-16', className)}>
-        <div className="bg-card text-card-foreground p-6 border shadow rounded flex flex-col gap-6">
+        <div className="bg-card text-card-foreground p-6 border shadow rounded-lg flex flex-col gap-6">
           <div className="flex flex-col justify-start gap-1">
             {heading && (
               <div className="prose md:prose-md dark:prose-invert">
