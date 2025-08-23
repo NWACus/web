7d3a91a5882d9a180debe8127257ef81ecee4d4b
rchlfryn <rachelafryan@gmail.com>
2025-08-23 14:29:55 -0700

    Add migration for sponsors

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -9,6 +9,7 @@ import * as migration_20250818_061729_generic_embed_block from './20250818_06172
 import * as migration_20250820_004406_home_page_global_collection from './20250820_004406_home_page_global_collection'
 import * as migration_20250821_205336_blog_list_block from './20250821_205336_blog_list_block'
 import * as migration_20250823_200437_add_single_blog_post_block from './20250823_200437_add_single_blog_post_block'
+import * as migration_20250823_212945_add_sponsors from './20250823_212945_add_sponsors'
 
 export const migrations = [
   {
@@ -65,5 +66,10 @@ export const migrations = [
     up: migration_20250823_200437_add_single_blog_post_block.up,
     down: migration_20250823_200437_add_single_blog_post_block.down,
     name: '20250823_200437_add_single_blog_post_block',
+  },
+  {
+    up: migration_20250823_212945_add_sponsors.up,
+    down: migration_20250823_212945_add_sponsors.down,
+    name: '20250823_212945_add_sponsors',
   },
 ]
