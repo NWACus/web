78c439ace5d8d4eb858660dcc267abb28ed67e93
rchlfryn <rachelafryan@gmail.com>
2025-08-28 14:10:43 -0700

    Update migration

--- src/migrations/20250828_211035_add_sponsors.ts
+++ src/migrations/20250828_211035_add_sponsors.ts
@@ -61,47 +61,6 @@ export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
   await db.run(sql`CREATE INDEX \`sponsors_photo_idx\` ON \`sponsors\` (\`photo_id\`);`)
   await db.run(sql`CREATE INDEX \`sponsors_updated_at_idx\` ON \`sponsors\` (\`updated_at\`);`)
   await db.run(sql`CREATE INDEX \`sponsors_created_at_idx\` ON \`sponsors\` (\`created_at\`);`)
-  await db.run(sql`CREATE TABLE \`home_pages_blocks_sponsors_block\` (
-  	\`_order\` integer NOT NULL,
-  	\`_parent_id\` integer NOT NULL,
-  	\`_path\` text NOT NULL,
-  	\`id\` text PRIMARY KEY NOT NULL,
-  	\`title\` text,
-  	\`background_color\` text DEFAULT 'white',
-  	\`block_name\` text,
-  	FOREIGN KEY (\`_parent_id\`) REFERENCES \`home_pages\`(\`id\`) ON UPDATE no action ON DELETE cascade
-  );
-  `)
-  await db.run(
-    sql`CREATE INDEX \`home_pages_blocks_sponsors_block_order_idx\` ON \`home_pages_blocks_sponsors_block\` (\`_order\`);`,
-  )
-  await db.run(
-    sql`CREATE INDEX \`home_pages_blocks_sponsors_block_parent_id_idx\` ON \`home_pages_blocks_sponsors_block\` (\`_parent_id\`);`,
-  )
-  await db.run(
-    sql`CREATE INDEX \`home_pages_blocks_sponsors_block_path_idx\` ON \`home_pages_blocks_sponsors_block\` (\`_path\`);`,
-  )
-  await db.run(sql`CREATE TABLE \`_home_pages_v_blocks_sponsors_block\` (
-  	\`_order\` integer NOT NULL,
-  	\`_parent_id\` integer NOT NULL,
-  	\`_path\` text NOT NULL,
-  	\`id\` integer PRIMARY KEY NOT NULL,
-  	\`title\` text,
-  	\`background_color\` text DEFAULT 'white',
-  	\`_uuid\` text,
-  	\`block_name\` text,
-  	FOREIGN KEY (\`_parent_id\`) REFERENCES \`_home_pages_v\`(\`id\`) ON UPDATE no action ON DELETE cascade
-  );
-  `)
-  await db.run(
-    sql`CREATE INDEX \`_home_pages_v_blocks_sponsors_block_order_idx\` ON \`_home_pages_v_blocks_sponsors_block\` (\`_order\`);`,
-  )
-  await db.run(
-    sql`CREATE INDEX \`_home_pages_v_blocks_sponsors_block_parent_id_idx\` ON \`_home_pages_v_blocks_sponsors_block\` (\`_parent_id\`);`,
-  )
-  await db.run(
-    sql`CREATE INDEX \`_home_pages_v_blocks_sponsors_block_path_idx\` ON \`_home_pages_v_blocks_sponsors_block\` (\`_path\`);`,
-  )
   await db.run(sql`ALTER TABLE \`pages_rels\` ADD \`sponsors_id\` integer REFERENCES sponsors(id);`)
   await db.run(
     sql`CREATE INDEX \`pages_rels_sponsors_id_idx\` ON \`pages_rels\` (\`sponsors_id\`);`,
@@ -111,18 +70,6 @@ export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
   )
   await db.run(
     sql`CREATE INDEX \`_pages_v_rels_sponsors_id_idx\` ON \`_pages_v_rels\` (\`sponsors_id\`);`,
-  )
-  await db.run(
-    sql`ALTER TABLE \`home_pages_rels\` ADD \`sponsors_id\` integer REFERENCES sponsors(id);`,
-  )
-  await db.run(
-    sql`CREATE INDEX \`home_pages_rels_sponsors_id_idx\` ON \`home_pages_rels\` (\`sponsors_id\`);`,
-  )
-  await db.run(
-    sql`ALTER TABLE \`_home_pages_v_rels\` ADD \`sponsors_id\` integer REFERENCES sponsors(id);`,
-  )
-  await db.run(
-    sql`CREATE INDEX \`_home_pages_v_rels_sponsors_id_idx\` ON \`_home_pages_v_rels\` (\`sponsors_id\`);`,
   )
   await db.run(
     sql`ALTER TABLE \`payload_locked_documents_rels\` ADD \`sponsors_id\` integer REFERENCES sponsors(id);`,
@@ -136,8 +83,6 @@ export async function down({ db, payload, req }: MigrateDownArgs): Promise<void>
   await db.run(sql`DROP TABLE \`pages_blocks_sponsors_block\`;`)
   await db.run(sql`DROP TABLE \`_pages_v_blocks_sponsors_block\`;`)
   await db.run(sql`DROP TABLE \`sponsors\`;`)
-  await db.run(sql`DROP TABLE \`home_pages_blocks_sponsors_block\`;`)
-  await db.run(sql`DROP TABLE \`_home_pages_v_blocks_sponsors_block\`;`)
   await db.run(sql`PRAGMA foreign_keys=OFF;`)
   await db.run(sql`CREATE TABLE \`__new_pages_rels\` (
   	\`id\` integer PRIMARY KEY NOT NULL,
@@ -193,66 +138,6 @@ export async function down({ db, payload, req }: MigrateDownArgs): Promise<void>
   )
   await db.run(
     sql`CREATE INDEX \`_pages_v_rels_pages_id_idx\` ON \`_pages_v_rels\` (\`pages_id\`);`,
-  )
-  await db.run(sql`CREATE TABLE \`__new_home_pages_rels\` (
-  	\`id\` integer PRIMARY KEY NOT NULL,
-  	\`order\` integer,
-  	\`parent_id\` integer NOT NULL,
-  	\`path\` text NOT NULL,
-  	\`pages_id\` integer,
-  	\`posts_id\` integer,
-  	FOREIGN KEY (\`parent_id\`) REFERENCES \`home_pages\`(\`id\`) ON UPDATE no action ON DELETE cascade,
-  	FOREIGN KEY (\`pages_id\`) REFERENCES \`pages\`(\`id\`) ON UPDATE no action ON DELETE cascade,
-  	FOREIGN KEY (\`posts_id\`) REFERENCES \`posts\`(\`id\`) ON UPDATE no action ON DELETE cascade
-  );
-  `)
-  await db.run(
-    sql`INSERT INTO \`__new_home_pages_rels\`("id", "order", "parent_id", "path", "pages_id", "posts_id") SELECT "id", "order", "parent_id", "path", "pages_id", "posts_id" FROM \`home_pages_rels\`;`,
-  )
-  await db.run(sql`DROP TABLE \`home_pages_rels\`;`)
-  await db.run(sql`ALTER TABLE \`__new_home_pages_rels\` RENAME TO \`home_pages_rels\`;`)
-  await db.run(sql`CREATE INDEX \`home_pages_rels_order_idx\` ON \`home_pages_rels\` (\`order\`);`)
-  await db.run(
-    sql`CREATE INDEX \`home_pages_rels_parent_idx\` ON \`home_pages_rels\` (\`parent_id\`);`,
-  )
-  await db.run(sql`CREATE INDEX \`home_pages_rels_path_idx\` ON \`home_pages_rels\` (\`path\`);`)
-  await db.run(
-    sql`CREATE INDEX \`home_pages_rels_pages_id_idx\` ON \`home_pages_rels\` (\`pages_id\`);`,
-  )
-  await db.run(
-    sql`CREATE INDEX \`home_pages_rels_posts_id_idx\` ON \`home_pages_rels\` (\`posts_id\`);`,
-  )
-  await db.run(sql`CREATE TABLE \`__new__home_pages_v_rels\` (
-  	\`id\` integer PRIMARY KEY NOT NULL,
-  	\`order\` integer,
-  	\`parent_id\` integer NOT NULL,
-  	\`path\` text NOT NULL,
-  	\`pages_id\` integer,
-  	\`posts_id\` integer,
-  	FOREIGN KEY (\`parent_id\`) REFERENCES \`_home_pages_v\`(\`id\`) ON UPDATE no action ON DELETE cascade,
-  	FOREIGN KEY (\`pages_id\`) REFERENCES \`pages\`(\`id\`) ON UPDATE no action ON DELETE cascade,
-  	FOREIGN KEY (\`posts_id\`) REFERENCES \`posts\`(\`id\`) ON UPDATE no action ON DELETE cascade
-  );
-  `)
-  await db.run(
-    sql`INSERT INTO \`__new__home_pages_v_rels\`("id", "order", "parent_id", "path", "pages_id", "posts_id") SELECT "id", "order", "parent_id", "path", "pages_id", "posts_id" FROM \`_home_pages_v_rels\`;`,
-  )
-  await db.run(sql`DROP TABLE \`_home_pages_v_rels\`;`)
-  await db.run(sql`ALTER TABLE \`__new__home_pages_v_rels\` RENAME TO \`_home_pages_v_rels\`;`)
-  await db.run(
-    sql`CREATE INDEX \`_home_pages_v_rels_order_idx\` ON \`_home_pages_v_rels\` (\`order\`);`,
-  )
-  await db.run(
-    sql`CREATE INDEX \`_home_pages_v_rels_parent_idx\` ON \`_home_pages_v_rels\` (\`parent_id\`);`,
-  )
-  await db.run(
-    sql`CREATE INDEX \`_home_pages_v_rels_path_idx\` ON \`_home_pages_v_rels\` (\`path\`);`,
-  )
-  await db.run(
-    sql`CREATE INDEX \`_home_pages_v_rels_pages_id_idx\` ON \`_home_pages_v_rels\` (\`pages_id\`);`,
-  )
-  await db.run(
-    sql`CREATE INDEX \`_home_pages_v_rels_posts_id_idx\` ON \`_home_pages_v_rels\` (\`posts_id\`);`,
   )
   await db.run(sql`CREATE TABLE \`__new_payload_locked_documents_rels\` (
   	\`id\` integer PRIMARY KEY NOT NULL,
--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -10,7 +10,7 @@ import * as migration_20250820_004406_home_page_global_collection from './202508
 import * as migration_20250821_205336_blog_list_block from './20250821_205336_blog_list_block'
 import * as migration_20250823_200437_add_single_blog_post_block from './20250823_200437_add_single_blog_post_block'
 import * as migration_20250824_200244_add_footer_form from './20250824_200244_add_footer_form'
-import * as migration_20250828_180209_add_sponsors from './20250828_180209_add_sponsors'
+import * as migration_20250828_211035_add_sponsors from './20250828_211035_add_sponsors'
 
 export const migrations = [
   {
@@ -74,8 +74,8 @@ export const migrations = [
     name: '20250824_200244_add_footer_form',
   },
   {
-    up: migration_20250828_180209_add_sponsors.up,
-    down: migration_20250828_180209_add_sponsors.down,
-    name: '20250828_180209_add_sponsors',
+    up: migration_20250828_211035_add_sponsors.up,
+    down: migration_20250828_211035_add_sponsors.down,
+    name: '20250828_211035_add_sponsors',
   },
 ]
