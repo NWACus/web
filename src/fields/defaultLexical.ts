d1a94617de9ff9e5cb64d0b76490e1f25fbc9f01
rchlfryn <rachelafryan@gmail.com>
2025-08-28 22:32:54 -0700

    Update validation

--- src/fields/defaultLexical.ts
+++ src/fields/defaultLexical.ts
@@ -10,12 +10,13 @@ import {
   ItalicFeature,
   lexicalEditor,
   LinkFeature,
+  LinkFields,
   OrderedListFeature,
   ParagraphFeature,
   UnderlineFeature,
   UnorderedListFeature,
 } from '@payloadcms/richtext-lexical'
-import { Config } from 'payload'
+import { Config, TextFieldSingleValidation } from 'payload'
 
 export const defaultLexical: Config['editor'] = lexicalEditor({
   features: () => {
@@ -41,10 +42,16 @@ export const defaultLexical: Config['editor'] = lexicalEditor({
               name: 'url',
               type: 'text',
               admin: {
-                condition: ({ linkType }) => linkType !== 'internal',
+                condition: (_data, siblingData) => siblingData?.linkType !== 'internal',
               },
               label: ({ t }) => t('fields:enterURL'),
               required: true,
+              validate: ((value, options) => {
+                if ((options?.siblingData as LinkFields)?.linkType === 'internal') {
+                  return true // no validation needed, as no url should exist for internal links
+                }
+                return value ? true : 'URL is required'
+              }) as TextFieldSingleValidation,
             },
           ]
         },
