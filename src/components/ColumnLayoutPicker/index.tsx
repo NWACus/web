deaad1521bea6f7f5f5cddd22a537d7d9fc82e1e
rchlfryn <rachelafryan@gmail.com>
2025-09-06 11:14:50 -0700

    Only render possible layout per number of columns & update icons to be correct

--- src/components/ColumnLayoutPicker/index.tsx
+++ src/components/ColumnLayoutPicker/index.tsx
@@ -1,24 +1,24 @@
 'use client'
 
 import { ToggleGroup, ToggleGroupItem } from '@/components/ui/toggle-group'
-import { FieldLabel, useField } from '@payloadcms/ui'
-import { Columns2, Columns3, Columns4 } from 'lucide-react'
+import { FieldLabel, useField, useFormFields } from '@payloadcms/ui'
+import { Columns2, Columns3, Columns4, Square } from 'lucide-react'
 import { Field, SelectFieldClientProps } from 'payload'
+import { Columns112, Columns12, Columns121, Columns21, Columns211 } from './icons'
 type ColumnLayoutPickerProps = {
   sb: Field
 } & SelectFieldClientProps
 const ColumnLayoutPicker = (props: ColumnLayoutPickerProps) => {
-  // TODO: update icons to be correct
   const layoutOptions = [
-    { label: 'full', value: '1_1', icon: Columns2 },
+    { label: 'full', value: '1_1', icon: Square },
     { label: '1:1', value: '2_11', icon: Columns2 },
     { label: '1:1:1', value: '3_111', icon: Columns3 },
-    { label: '1:2', value: '2_12', icon: Columns3 },
-    { label: '2:1', value: '2_21', icon: Columns3 },
+    { label: '1:2', value: '2_12', icon: Columns12 },
+    { label: '2:1', value: '2_21', icon: Columns21 },
     { label: '1:1:1:1', value: '4_1111', icon: Columns4 },
-    { label: '1:1:2', value: '3_112', icon: Columns4 },
-    { label: '1:2:1', value: '3_121', icon: Columns4 },
-    { label: '2:1:1', value: '3_211', icon: Columns4 },
+    { label: '1:1:2', value: '3_112', icon: Columns112 },
+    { label: '1:2:1', value: '3_121', icon: Columns121 },
+    { label: '2:1:1', value: '3_211', icon: Columns211 },
   ]
   const { path, field } = props
 
@@ -26,10 +26,14 @@ const ColumnLayoutPicker = (props: ColumnLayoutPickerProps) => {
 
   const currentValue = value || 'one_1'
 
-  // const fields = useFormFields(([fields]) => fields)
-  // const totalColumns = fields.columns?.value
-  // console.log(fields)
-  // console.log('path', path)
+  const fields = useFormFields(([fields]) => fields)
+  const columnsPath = path.replace(/layout$/, 'columns')
+  const numOfCols = fields[columnsPath].rows?.length || 0
+
+  const filteredLayoutOptions = layoutOptions.filter((option) => {
+    const colCount = parseInt(option.value.split('_')[0])
+    return colCount <= numOfCols
+  })
 
   return (
     <div className="flex items-start mb-6">
@@ -41,8 +45,7 @@ const ColumnLayoutPicker = (props: ColumnLayoutPickerProps) => {
         variant="outline"
         className="justify-start"
       >
-        {/* TODO only render possible layout per # of columns */}
-        {layoutOptions.map((option) => {
+        {filteredLayoutOptions.map((option) => {
           const Icon = option.icon
           return (
             <ToggleGroupItem
@@ -51,7 +54,7 @@ const ColumnLayoutPicker = (props: ColumnLayoutPickerProps) => {
               aria-label={option.label}
               className="cursor-pointer"
             >
-              <Icon />
+              <Icon style={{ width: '20px', height: '20px' }} />
             </ToggleGroupItem>
           )
         })}
