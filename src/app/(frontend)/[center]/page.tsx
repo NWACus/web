1f5f2975dcf24ef01bc538e74fe8f14b4abeea11
Kellen Busby <kellenbusby@gmail.com>
2025-09-22 18:57:04 -0400

    fixing warning widget typo - window key was wrong + updated name of widget to be plural to match

--- src/app/(frontend)/[center]/page.tsx
+++ src/app/(frontend)/[center]/page.tsx
@@ -48,7 +48,7 @@ export default async function Page({ params }: Args) {
     <>
       <NACWidget
         center={center}
-        widget="warning"
+        widget="warnings"
         widgetsVersion={version}
         widgetsBaseUrl={baseUrl}
       />
