0ab4a33bd5702c1773530f6f099124bfbabb6f31
rchlfryn <rachelafryan@gmail.com>
2025-10-02 16:45:07 -0700

    Change label and title

--- src/components/ViewDocumentButton/index.tsx
+++ src/components/ViewDocumentButton/index.tsx
@@ -9,11 +9,11 @@ export const ViewDocumentButton = (props: { url: string }) => {
   }
   return (
     <button
-      aria-label="View Page Button"
+      aria-label="View published"
       className="view-page-btn"
       id="preview-button"
       onClick={openNewTab}
-      title="View Page Button"
+      title="View published"
       type="button"
     >
       <Globe width={16} height={16} className="icon" />
