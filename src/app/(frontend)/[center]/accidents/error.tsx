b277a13c5b5b52a077adc2630f3a16ef930b8307
rchlfryn <rachelafryan@gmail.com>
2025-10-14 14:07:30 -0700

    Make observations page dynamic

--- src/app/(frontend)/[center]/accidents/error.tsx
+++
@@ -1,5 +0,0 @@
-'use client'
-
-import { ErrorBoundary } from '@/components/ErrorBoundary/ErrorBoundary'
-
-export default ErrorBoundary
--- src/app/(frontend)/[center]/observations/page.tsx
+++ src/app/(frontend)/[center]/observations/page.tsx
@@ -29,14 +29,16 @@ export async function generateStaticParams() {
 
 type Args = {
   params: Promise<PathArgs>
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>
 }
 
 type PathArgs = {
   center: string
 }
 
-export default async function Page({ params }: Args) {
+export default async function Page({ params, searchParams }: Args) {
   const { center } = await params
+  const search = await searchParams
 
   const avalancheCenterPlatforms = await getAvalancheCenterPlatforms(center)
 
@@ -46,9 +48,22 @@ export default async function Page({ params }: Args) {
 
   const { version, baseUrl } = await getNACWidgetsConfig()
 
+  const queryParams = new URLSearchParams()
+
+  Object.entries(search).forEach(([key, value]) => {
+    if (value !== undefined) {
+      const stringValue = Array.isArray(value) ? value[0] : value
+      const decodedValue = decodeURIComponent(stringValue)
+      queryParams.set(key, decodedValue)
+    }
+  })
+
+  const queryString = queryParams.toString()
+  const initialHash = `/view/observations${queryString ? `?${queryString}` : ''}`
+  const cleanUrl = queryString ? `/observations` : undefined
   return (
     <>
-      <WidgetHashHandler initialHash="/view/observations" />
+      <WidgetHashHandler initialHash={initialHash} cleanUrl={cleanUrl} />
       <div className="flex flex-col gap-4">
         <div className="container">
           <div className="flex justify-between items-center gap-4 prose dark:prose-invert max-w-none">
