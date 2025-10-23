c59a6d38f6136bb0271cf67ac78d78d9b61a83b2
Kellen Busby <kellenbusby@gmail.com>
2025-10-23 15:22:07 -0700

    adding the correct default values to avoid JSON parsing error

--- src/blocks/Content/config.ts
+++ src/blocks/Content/config.ts
@@ -75,8 +75,28 @@ export const Content: Block = {
         plural: 'Columns',
         singular: 'Column',
       },
+      defaultValue: [
+        {
+          root: {
+            children: [
+              {
+                children: [],
+                direction: null,
+                format: '',
+                indent: 0,
+                type: 'paragraph',
+                version: 1,
+              },
+            ],
+            direction: null,
+            format: '',
+            indent: 0,
+            type: 'root',
+            version: 1,
+          },
+        },
+      ],
       required: true,
-      defaultValue: [{}],
       type: 'array',
       admin: {
         initCollapsed: false,
