97e591f492801eae3558a7aa31f4b3bdec29bae7
Kellen Busby <kellenbusby@gmail.com>
2025-10-24 14:31:54 -0700

    defaultValue for the columns can be empty to avoid duplicate columns

--- src/blocks/Content/config.ts
+++ src/blocks/Content/config.ts
@@ -75,27 +75,7 @@ export const Content: Block = {
         plural: 'Columns',
         singular: 'Column',
       },
-      defaultValue: [
-        {
-          root: {
-            children: [
-              {
-                children: [],
-                direction: null,
-                format: '',
-                indent: 0,
-                type: 'paragraph',
-                version: 1,
-              },
-            ],
-            direction: null,
-            format: '',
-            indent: 0,
-            type: 'root',
-            version: 1,
-          },
-        },
-      ],
+      defaultValue: [{}],
       required: true,
       type: 'array',
       admin: {
