1b35114d70970bd570c6e572ecefdb8fc67020c3
Kellen Busby <kellenbusby@gmail.com>
2025-10-27 12:27:53 -0700

    using a create new column, copy, delete, rename approach instead of the same pattern for tables

--- src/migrations/20251024_203935_content_block_default_value.ts
+++ src/migrations/20251024_203935_content_block_default_value.ts
@@ -1,181 +1,124 @@
 import { MigrateDownArgs, MigrateUpArgs, sql } from '@payloadcms/db-sqlite'
 
-export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
-  await db.run(sql`CREATE TABLE \`__new_home_pages_blocks_content_columns\` (
-  	\`_order\` integer NOT NULL,
-  	\`_parent_id\` text NOT NULL,
-  	\`id\` text PRIMARY KEY NOT NULL,
-  	\`rich_text\` text DEFAULT '{"root":{"children":[{"children":[],"direction":null,"format":"","indent":0,"type":"paragraph","version":1}],"direction":null,"format":"","indent":0,"type":"root","version":1}}',
-  	FOREIGN KEY (\`_parent_id\`) REFERENCES \`home_pages_blocks_content\`(\`id\`) ON UPDATE no action ON DELETE cascade
-  );
-  `)
+export async function up({ db }: MigrateUpArgs): Promise<void> {
+  const DEFAULT_LEXICAL_VALUE =
+    '{"root":{"type":"root","format":"","indent":0,"version":1,"children":[{"type":"paragraph","format":"","indent":0,"version":1,"children":[],"direction":"ltr","textStyle":"","textFormat":0}],"direction":"ltr"}}'
+
+  // home_pages_blocks_content_columns
   await db.run(
-    sql`INSERT INTO \`__new_home_pages_blocks_content_columns\`("_order", "_parent_id", "id", "rich_text") SELECT "_order", "_parent_id", "id", "rich_text" FROM \`home_pages_blocks_content_columns\`;`,
-  )
-  await db.run(sql`DROP TABLE \`home_pages_blocks_content_columns\`;`)
-  await db.run(
-    sql`ALTER TABLE \`__new_home_pages_blocks_content_columns\` RENAME TO \`home_pages_blocks_content_columns\`;`,
+    sql.raw(
+      `ALTER TABLE \`home_pages_blocks_content_columns\` ADD COLUMN \`__new_rich_text\` text DEFAULT '${DEFAULT_LEXICAL_VALUE}';`,
+    ),
   )
   await db.run(
-    sql`CREATE INDEX \`home_pages_blocks_content_columns_order_idx\` ON \`home_pages_blocks_content_columns\` (\`_order\`);`,
+    sql.raw(
+      `UPDATE \`home_pages_blocks_content_columns\` SET \`__new_rich_text\` = COALESCE(\`rich_text\`, '${DEFAULT_LEXICAL_VALUE}');`,
+    ),
+  )
+  await db.run(sql`ALTER TABLE \`home_pages_blocks_content_columns\` DROP COLUMN \`rich_text\`;`)
+  await db.run(
+    sql`ALTER TABLE \`home_pages_blocks_content_columns\` RENAME COLUMN \`__new_rich_text\` TO \`rich_text\`;`,
+  )
+
+  // _home_pages_v_blocks_content_columns
+  await db.run(
+    sql.raw(
+      `ALTER TABLE \`_home_pages_v_blocks_content_columns\` ADD COLUMN \`__new_rich_text\` text DEFAULT '${DEFAULT_LEXICAL_VALUE}';`,
+    ),
   )
   await db.run(
-    sql`CREATE INDEX \`home_pages_blocks_content_columns_parent_id_idx\` ON \`home_pages_blocks_content_columns\` (\`_parent_id\`);`,
+    sql.raw(
+      `UPDATE \`_home_pages_v_blocks_content_columns\` SET \`__new_rich_text\` = COALESCE(\`rich_text\`, '${DEFAULT_LEXICAL_VALUE}');`,
+    ),
   )
-  await db.run(sql`CREATE TABLE \`__new__home_pages_v_blocks_content_columns\` (
-  	\`_order\` integer NOT NULL,
-  	\`_parent_id\` integer NOT NULL,
-  	\`id\` integer PRIMARY KEY NOT NULL,
-  	\`rich_text\` text DEFAULT '{"root":{"children":[{"children":[],"direction":null,"format":"","indent":0,"type":"paragraph","version":1}],"direction":null,"format":"","indent":0,"type":"root","version":1}}',
-  	\`_uuid\` text,
-  	FOREIGN KEY (\`_parent_id\`) REFERENCES \`_home_pages_v_blocks_content\`(\`id\`) ON UPDATE no action ON DELETE cascade
-  );
-  `)
+  await db.run(sql`ALTER TABLE \`_home_pages_v_blocks_content_columns\` DROP COLUMN \`rich_text\`;`)
   await db.run(
-    sql`INSERT INTO \`__new__home_pages_v_blocks_content_columns\`("_order", "_parent_id", "id", "rich_text", "_uuid") SELECT "_order", "_parent_id", "id", "rich_text", "_uuid" FROM \`_home_pages_v_blocks_content_columns\`;`,
+    sql`ALTER TABLE \`_home_pages_v_blocks_content_columns\` RENAME COLUMN \`__new_rich_text\` TO \`rich_text\`;`,
   )
-  await db.run(sql`DROP TABLE \`_home_pages_v_blocks_content_columns\`;`)
+
+  // pages_blocks_content_columns
   await db.run(
-    sql`ALTER TABLE \`__new__home_pages_v_blocks_content_columns\` RENAME TO \`_home_pages_v_blocks_content_columns\`;`,
+    sql.raw(
+      `ALTER TABLE \`pages_blocks_content_columns\` ADD COLUMN \`__new_rich_text\` text DEFAULT '${DEFAULT_LEXICAL_VALUE}';`,
+    ),
   )
   await db.run(
-    sql`CREATE INDEX \`_home_pages_v_blocks_content_columns_order_idx\` ON \`_home_pages_v_blocks_content_columns\` (\`_order\`);`,
+    sql.raw(
+      `UPDATE \`pages_blocks_content_columns\` SET \`__new_rich_text\` = COALESCE(\`rich_text\`, '${DEFAULT_LEXICAL_VALUE}');`,
+    ),
+  )
+  await db.run(sql`ALTER TABLE \`pages_blocks_content_columns\` DROP COLUMN \`rich_text\`;`)
+  await db.run(
+    sql`ALTER TABLE \`pages_blocks_content_columns\` RENAME COLUMN \`__new_rich_text\` TO \`rich_text\`;`,
+  )
+
+  // _pages_v_blocks_content_columns
+  await db.run(
+    sql.raw(
+      `ALTER TABLE \`_pages_v_blocks_content_columns\` ADD COLUMN \`__new_rich_text\` text DEFAULT '${DEFAULT_LEXICAL_VALUE}';`,
+    ),
   )
   await db.run(
-    sql`CREATE INDEX \`_home_pages_v_blocks_content_columns_parent_id_idx\` ON \`_home_pages_v_blocks_content_columns\` (\`_parent_id\`);`,
+    sql.raw(
+      `UPDATE \`_pages_v_blocks_content_columns\` SET \`__new_rich_text\` = COALESCE(\`rich_text\`, '${DEFAULT_LEXICAL_VALUE}');`,
+    ),
   )
-  await db.run(sql`CREATE TABLE \`__new_pages_blocks_content_columns\` (
-  	\`_order\` integer NOT NULL,
-  	\`_parent_id\` text NOT NULL,
-  	\`id\` text PRIMARY KEY NOT NULL,
-  	\`rich_text\` text DEFAULT '{"root":{"children":[{"children":[],"direction":null,"format":"","indent":0,"type":"paragraph","version":1}],"direction":null,"format":"","indent":0,"type":"root","version":1}}',
-  	FOREIGN KEY (\`_parent_id\`) REFERENCES \`pages_blocks_content\`(\`id\`) ON UPDATE no action ON DELETE cascade
-  );
-  `)
+  await db.run(sql`ALTER TABLE \`_pages_v_blocks_content_columns\` DROP COLUMN \`rich_text\`;`)
   await db.run(
-    sql`INSERT INTO \`__new_pages_blocks_content_columns\`("_order", "_parent_id", "id", "rich_text") SELECT "_order", "_parent_id", "id", "rich_text" FROM \`pages_blocks_content_columns\`;`,
-  )
-  await db.run(sql`DROP TABLE \`pages_blocks_content_columns\`;`)
-  await db.run(
-    sql`ALTER TABLE \`__new_pages_blocks_content_columns\` RENAME TO \`pages_blocks_content_columns\`;`,
-  )
-  await db.run(
-    sql`CREATE INDEX \`pages_blocks_content_columns_order_idx\` ON \`pages_blocks_content_columns\` (\`_order\`);`,
-  )
-  await db.run(
-    sql`CREATE INDEX \`pages_blocks_content_columns_parent_id_idx\` ON \`pages_blocks_content_columns\` (\`_parent_id\`);`,
-  )
-  await db.run(sql`CREATE TABLE \`__new__pages_v_blocks_content_columns\` (
-  	\`_order\` integer NOT NULL,
-  	\`_parent_id\` integer NOT NULL,
-  	\`id\` integer PRIMARY KEY NOT NULL,
-  	\`rich_text\` text DEFAULT '{"root":{"children":[{"children":[],"direction":null,"format":"","indent":0,"type":"paragraph","version":1}],"direction":null,"format":"","indent":0,"type":"root","version":1}}',
-  	\`_uuid\` text,
-  	FOREIGN KEY (\`_parent_id\`) REFERENCES \`_pages_v_blocks_content\`(\`id\`) ON UPDATE no action ON DELETE cascade
-  );
-  `)
-  await db.run(
-    sql`INSERT INTO \`__new__pages_v_blocks_content_columns\`("_order", "_parent_id", "id", "rich_text", "_uuid") SELECT "_order", "_parent_id", "id", "rich_text", "_uuid" FROM \`_pages_v_blocks_content_columns\`;`,
-  )
-  await db.run(sql`DROP TABLE \`_pages_v_blocks_content_columns\`;`)
-  await db.run(
-    sql`ALTER TABLE \`__new__pages_v_blocks_content_columns\` RENAME TO \`_pages_v_blocks_content_columns\`;`,
-  )
-  await db.run(
-    sql`CREATE INDEX \`_pages_v_blocks_content_columns_order_idx\` ON \`_pages_v_blocks_content_columns\` (\`_order\`);`,
-  )
-  await db.run(
-    sql`CREATE INDEX \`_pages_v_blocks_content_columns_parent_id_idx\` ON \`_pages_v_blocks_content_columns\` (\`_parent_id\`);`,
+    sql`ALTER TABLE \`_pages_v_blocks_content_columns\` RENAME COLUMN \`__new_rich_text\` TO \`rich_text\`;`,
   )
 }
 
-export async function down({ db, payload, req }: MigrateDownArgs): Promise<void> {
-  await db.run(sql`PRAGMA foreign_keys=OFF;`)
-  await db.run(sql`CREATE TABLE \`__new_home_pages_blocks_content_columns\` (
-  	\`_order\` integer NOT NULL,
-  	\`_parent_id\` text NOT NULL,
-  	\`id\` text PRIMARY KEY NOT NULL,
-  	\`rich_text\` text,
-  	FOREIGN KEY (\`_parent_id\`) REFERENCES \`home_pages_blocks_content\`(\`id\`) ON UPDATE no action ON DELETE cascade
-  );
-  `)
+export async function down({ db }: MigrateDownArgs): Promise<void> {
+  // Note: This down migration removes the DEFAULT constraint but does not restore
+  // originally NULL values. The up migration permanently converts NULL to the default
+  // Lexical value, and there's no way to determine which values were originally NULL.
+
+  // home_pages_blocks_content_columns
   await db.run(
-    sql`INSERT INTO \`__new_home_pages_blocks_content_columns\`("_order", "_parent_id", "id", "rich_text") SELECT "_order", "_parent_id", "id", "rich_text" FROM \`home_pages_blocks_content_columns\`;`,
-  )
-  await db.run(sql`DROP TABLE \`home_pages_blocks_content_columns\`;`)
-  await db.run(
-    sql`ALTER TABLE \`__new_home_pages_blocks_content_columns\` RENAME TO \`home_pages_blocks_content_columns\`;`,
-  )
-  await db.run(sql`PRAGMA foreign_keys=ON;`)
-  await db.run(
-    sql`CREATE INDEX \`home_pages_blocks_content_columns_order_idx\` ON \`home_pages_blocks_content_columns\` (\`_order\`);`,
+    sql`ALTER TABLE \`home_pages_blocks_content_columns\` ADD COLUMN \`__new_rich_text\` text;`,
   )
   await db.run(
-    sql`CREATE INDEX \`home_pages_blocks_content_columns_parent_id_idx\` ON \`home_pages_blocks_content_columns\` (\`_parent_id\`);`,
+    sql`UPDATE \`home_pages_blocks_content_columns\` SET \`__new_rich_text\` = \`rich_text\`;`,
   )
-  await db.run(sql`CREATE TABLE \`__new__home_pages_v_blocks_content_columns\` (
-  	\`_order\` integer NOT NULL,
-  	\`_parent_id\` integer NOT NULL,
-  	\`id\` integer PRIMARY KEY NOT NULL,
-  	\`rich_text\` text,
-  	\`_uuid\` text,
-  	FOREIGN KEY (\`_parent_id\`) REFERENCES \`_home_pages_v_blocks_content\`(\`id\`) ON UPDATE no action ON DELETE cascade
-  );
-  `)
+  await db.run(sql`ALTER TABLE \`home_pages_blocks_content_columns\` DROP COLUMN \`rich_text\`;`)
   await db.run(
-    sql`INSERT INTO \`__new__home_pages_v_blocks_content_columns\`("_order", "_parent_id", "id", "rich_text", "_uuid") SELECT "_order", "_parent_id", "id", "rich_text", "_uuid" FROM \`_home_pages_v_blocks_content_columns\`;`,
+    sql`ALTER TABLE \`home_pages_blocks_content_columns\` RENAME COLUMN \`__new_rich_text\` TO \`rich_text\`;`,
   )
-  await db.run(sql`DROP TABLE \`_home_pages_v_blocks_content_columns\`;`)
+
+  // _home_pages_v_blocks_content_columns
   await db.run(
-    sql`ALTER TABLE \`__new__home_pages_v_blocks_content_columns\` RENAME TO \`_home_pages_v_blocks_content_columns\`;`,
+    sql`ALTER TABLE \`_home_pages_v_blocks_content_columns\` ADD COLUMN \`__new_rich_text\` text;`,
   )
   await db.run(
-    sql`CREATE INDEX \`_home_pages_v_blocks_content_columns_order_idx\` ON \`_home_pages_v_blocks_content_columns\` (\`_order\`);`,
+    sql`UPDATE \`_home_pages_v_blocks_content_columns\` SET \`__new_rich_text\` = \`rich_text\`;`,
+  )
+  await db.run(sql`ALTER TABLE \`_home_pages_v_blocks_content_columns\` DROP COLUMN \`rich_text\`;`)
+  await db.run(
+    sql`ALTER TABLE \`_home_pages_v_blocks_content_columns\` RENAME COLUMN \`__new_rich_text\` TO \`rich_text\`;`,
+  )
+
+  // pages_blocks_content_columns
+  await db.run(
+    sql`ALTER TABLE \`pages_blocks_content_columns\` ADD COLUMN \`__new_rich_text\` text;`,
   )
   await db.run(
-    sql`CREATE INDEX \`_home_pages_v_blocks_content_columns_parent_id_idx\` ON \`_home_pages_v_blocks_content_columns\` (\`_parent_id\`);`,
+    sql`UPDATE \`pages_blocks_content_columns\` SET \`__new_rich_text\` = \`rich_text\`;`,
   )
-  await db.run(sql`CREATE TABLE \`__new_pages_blocks_content_columns\` (
-  	\`_order\` integer NOT NULL,
-  	\`_parent_id\` text NOT NULL,
-  	\`id\` text PRIMARY KEY NOT NULL,
-  	\`rich_text\` text,
-  	FOREIGN KEY (\`_parent_id\`) REFERENCES \`pages_blocks_content\`(\`id\`) ON UPDATE no action ON DELETE cascade
-  );
-  `)
+  await db.run(sql`ALTER TABLE \`pages_blocks_content_columns\` DROP COLUMN \`rich_text\`;`)
   await db.run(
-    sql`INSERT INTO \`__new_pages_blocks_content_columns\`("_order", "_parent_id", "id", "rich_text") SELECT "_order", "_parent_id", "id", "rich_text" FROM \`pages_blocks_content_columns\`;`,
+    sql`ALTER TABLE \`pages_blocks_content_columns\` RENAME COLUMN \`__new_rich_text\` TO \`rich_text\`;`,
   )
-  await db.run(sql`DROP TABLE \`pages_blocks_content_columns\`;`)
+
+  // _pages_v_blocks_content_columns
   await db.run(
-    sql`ALTER TABLE \`__new_pages_blocks_content_columns\` RENAME TO \`pages_blocks_content_columns\`;`,
+    sql`ALTER TABLE \`_pages_v_blocks_content_columns\` ADD COLUMN \`__new_rich_text\` text;`,
   )
   await db.run(
-    sql`CREATE INDEX \`pages_blocks_content_columns_order_idx\` ON \`pages_blocks_content_columns\` (\`_order\`);`,
+    sql`UPDATE \`_pages_v_blocks_content_columns\` SET \`__new_rich_text\` = \`rich_text\`;`,
   )
+  await db.run(sql`ALTER TABLE \`_pages_v_blocks_content_columns\` DROP COLUMN \`rich_text\`;`)
   await db.run(
-    sql`CREATE INDEX \`pages_blocks_content_columns_parent_id_idx\` ON \`pages_blocks_content_columns\` (\`_parent_id\`);`,
-  )
-  await db.run(sql`CREATE TABLE \`__new__pages_v_blocks_content_columns\` (
-  	\`_order\` integer NOT NULL,
-  	\`_parent_id\` integer NOT NULL,
-  	\`id\` integer PRIMARY KEY NOT NULL,
-  	\`rich_text\` text,
-  	\`_uuid\` text,
-  	FOREIGN KEY (\`_parent_id\`) REFERENCES \`_pages_v_blocks_content\`(\`id\`) ON UPDATE no action ON DELETE cascade
-  );
-  `)
-  await db.run(
-    sql`INSERT INTO \`__new__pages_v_blocks_content_columns\`("_order", "_parent_id", "id", "rich_text", "_uuid") SELECT "_order", "_parent_id", "id", "rich_text", "_uuid" FROM \`_pages_v_blocks_content_columns\`;`,
-  )
-  await db.run(sql`DROP TABLE \`_pages_v_blocks_content_columns\`;`)
-  await db.run(
-    sql`ALTER TABLE \`__new__pages_v_blocks_content_columns\` RENAME TO \`_pages_v_blocks_content_columns\`;`,
-  )
-  await db.run(
-    sql`CREATE INDEX \`_pages_v_blocks_content_columns_order_idx\` ON \`_pages_v_blocks_content_columns\` (\`_order\`);`,
-  )
-  await db.run(
-    sql`CREATE INDEX \`_pages_v_blocks_content_columns_parent_id_idx\` ON \`_pages_v_blocks_content_columns\` (\`_parent_id\`);`,
+    sql`ALTER TABLE \`_pages_v_blocks_content_columns\` RENAME COLUMN \`__new_rich_text\` TO \`rich_text\`;`,
   )
 }
