67380538a5b3764f0dd5dd50d0694b93d4f452bb
Kellen Busby <kellenbusby@gmail.com>
2025-11-03 11:02:57 -0800

    allowing users to always read tenants they have a role for + hiding tenants from the nav if user only has read access

--- src/collections/Tenants/access/byGlobalRoleOrTenantIds.ts
+++ src/collections/Tenants/access/byGlobalRoleOrTenantIds.ts
@@ -41,9 +41,58 @@ export const byGlobalRoleOrTenantIds: (method: ruleMethod) => Access =
     return false
   }
 
+export const byGlobalRoleOrTenantIdsWithOwnTenantsRead: (method: ruleMethod) => Access =
+  (method: ruleMethod): Access =>
+  (args) => {
+    if (!args.req.user) {
+      return false
+    }
+
+    const globalAccess = byGlobalRole(method, 'tenants')(args)
+    if (typeof globalAccess === 'boolean' ? globalAccess : true) {
+      // if globalAccess returned anything but 'false', pass it along
+      return globalAccess
+    }
+
+    const roleAssignments = roleAssignmentsForUser(args.req.payload.logger, args.req.user)
+    const matchingTenantIds = roleAssignments
+      .filter(
+        (assignment) =>
+          assignment.role &&
+          typeof assignment.role !== 'number' && // captured in the getter
+          assignment.role.rules.some(ruleMatches(method, 'tenants')),
+      )
+      .map((assignment) => assignment.tenant)
+      .filter((tenant) => !!tenant && typeof tenant !== 'number') // captured in the getter
+      .map((tenant) => tenant.id)
+
+    if (matchingTenantIds.length > 0) {
+      return {
+        id: {
+          in: matchingTenantIds,
+        },
+      }
+    }
+
+    const tenantIdsUserHasRoleAssignmentsFor = roleAssignments
+      .map((assignment) => assignment.tenant)
+      .filter((tenant) => !!tenant && typeof tenant !== 'number')
+      .map((tenant) => tenant.id)
+
+    if (tenantIdsUserHasRoleAssignmentsFor.length > 0) {
+      return {
+        id: {
+          in: tenantIdsUserHasRoleAssignmentsFor,
+        },
+      }
+    }
+
+    return false
+  }
+
 export const accessByGlobalRoleOrTenantIds: CollectionConfig['access'] = {
   create: byGlobalRoleOrTenantIds('create'),
-  read: byGlobalRoleOrTenantIds('read'),
+  read: byGlobalRoleOrTenantIdsWithOwnTenantsRead('read'),
   update: byGlobalRoleOrTenantIds('update'),
   delete: byGlobalRoleOrTenantIds('delete'),
 }
