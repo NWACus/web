2b1c6dd144f5c8bedf15c1959ae284f02b6a3c16
rchlfryn <rachelafryan@gmail.com>
2025-11-23 11:32:48 -0800

    Fix carousel for 6 sponsors on large screen

--- src/blocks/SponsorsBlock/components/Carousel.tsx
+++ src/blocks/SponsorsBlock/components/Carousel.tsx
@@ -9,7 +9,6 @@ import {
 } from '@/components/ui/carousel'
 import { Sponsor } from '@/payload-types'
 import getTextColorFromBgColor from '@/utilities/getTextColorFromBgColor'
-import { cn } from '@/utilities/ui'
 import Autoplay from 'embla-carousel-autoplay'
 
 export const SponsorsBlockCarousel = ({
@@ -22,22 +21,18 @@ export const SponsorsBlockCarousel = ({
   const carouselItemClasses: { [key: string]: string[] } = {
     1: [''],
     2: [''],
-    3: ['sm:basis-1/2'],
-    4: ['md:basis-1/3'],
-    5: ['md:basis-1/3 lg:basis-1/4'],
-    6: ['md:basis-1/3 lg:basis-1/4 lg:basis-1/5'],
+    3: [''],
+    4: ['sm:basis-1/2'],
+    5: ['sm:basis-1/2 md:basis-1/3'],
+    6: ['sm:basis-1/2 md:basis-1/3 lg:basis-1/4'],
+    7: ['sm:basis-1/2 md:basis-1/3 lg:basis-1/5'],
     default: ['basis-1/2 md:basis-1/3 lg:basis-1/6'],
   }
 
   const carouselItemClass = carouselItemClasses[sponsors.length] || carouselItemClasses['default']
   const borderColor = getTextColorFromBgColor(bgColorClass).replace('text-bg', 'border')
   return (
-    <div
-      className={cn({
-        'max-w-5/6 md:max-w-11/12': sponsors.length < 6,
-        'w-5/6 md:w-11/12': sponsors.length >= 6,
-      })}
-    >
+    <div className="w-full">
       <Carousel
         opts={{
           align: 'start',
