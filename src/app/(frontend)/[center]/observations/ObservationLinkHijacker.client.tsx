af99ebe7992f854ce766af58509ffc30be493ee6
rchlfryn <rachelafryan@gmail.com>
2026-01-09 12:37:34 -0800

    Remove observation disconnect (want continuous monitoring)

--- src/app/(frontend)/[center]/observations/ObservationLinkHijacker.client.tsx
+++ src/app/(frontend)/[center]/observations/ObservationLinkHijacker.client.tsx
@@ -67,7 +67,6 @@ export function ObservationLinkHijacker() {
             document.querySelector('#nac-obs-modal-controls a')
           ) {
             modifyLinks()
-            observer.disconnect()
           }
         }
       })
