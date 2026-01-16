d72211c5980a30d029caf97895f2a7e1c6d55b24
Kellen Busby <kellenbusby@gmail.com>
2026-01-15 16:01:35 -0800

    fixing default column name

--- src/collections/Media/index.ts
+++ src/collections/Media/index.ts
@@ -23,7 +23,7 @@ export const Media: CollectionConfig = {
     group: 'Content',
     hidden: ({ user }) =>
       !hasGlobalOrTenantRolePermission({ method: 'read', collection: 'media', user }),
-    defaultColumns: ['fileName', 'alt', 'url', 'width', 'height'],
+    defaultColumns: ['filename', 'alt', 'url', 'width', 'height'],
   },
   fields: [
     tenantField(),
