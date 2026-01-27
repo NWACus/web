efe50cd6119d5e7b34b3a4359fdaf662f1ede472
Kellen Busby <kellenbusby@gmail.com>
2026-01-27 12:23:41 -0800

    falling back to tenant cookie if tenant not set for some reason in tenant and id filter too

--- src/utilities/collectionFilters.ts
+++ src/utilities/collectionFilters.ts
@@ -28,11 +28,29 @@ export const getTenantFilter = ({ data, req }: FilterOptionsProps): Where => {
   return {}
 }
 
-export const getTenantAndIdFilter = ({ id, data }: FilterOptionsProps) => ({
-  id: {
-    not_in: [id],
-  },
-  tenant: {
-    equals: data.tenant,
-  },
-})
+export const getTenantAndIdFilter = ({ id, data, req }: FilterOptionsProps): Where => {
+  const idFilter = { id: { not_in: [id] } }
+
+  // If the document already has a tenant relationship set, use that
+  if (data.tenant) {
+    return {
+      ...idFilter,
+      tenant: {
+        equals: data.tenant,
+      },
+    }
+  }
+
+  // Otherwise, filter by tenant slug from cookie
+  const tenantSlug = getTenantSlugFromCookie(req.headers)
+  if (tenantSlug) {
+    return {
+      ...idFilter,
+      'tenant.slug': {
+        equals: tenantSlug,
+      },
+    }
+  }
+
+  return idFilter
+}
