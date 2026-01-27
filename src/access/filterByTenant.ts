01ddd7c353ed152305c188807793bc03b07b37dd
Kellen Busby <kellenbusby@gmail.com>
2026-01-27 12:25:32 -0800

    replacing deprecated type

--- src/access/filterByTenant.ts
+++ src/access/filterByTenant.ts
@@ -1,11 +1,11 @@
 import { getTenantSlugFromCookie } from '@/utilities/tenancy/getTenantFromCookie'
-import { BaseListFilter } from 'payload'
+import { BaseFilter } from 'payload'
 
 // filterByTenant implements per-tenant data filtering from the 'payload-tenant' cookie
 // by setting the base list filters that users can add to but not remove from. Access
 // control defines the set of data to be filtered, so this filter is on-top and has no
 // risk of letting users see data they cannot access.
-export const filterByTenant: BaseListFilter = async ({ req }) => {
+export const filterByTenant: BaseFilter = async ({ req }) => {
   const tenantSlug = getTenantSlugFromCookie(req.headers)
 
   if (tenantSlug) {
