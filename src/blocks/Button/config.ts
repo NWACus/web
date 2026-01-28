474ba264917fc5613996a1208c073682c05c6ee2
rchlfryn <rachelafryan@gmail.com>
2026-01-28 11:32:17 -0800

    Update thumbnails for button, single event and post

--- src/blocks/Button/config.ts
+++ src/blocks/Button/config.ts
@@ -4,5 +4,6 @@ import type { Block } from 'payload'
 export const ButtonBlock: Block = {
   slug: 'buttonBlock',
   interfaceName: 'ButtonBlock',
+  imageURL: '/thumbnail/ButtonThumbnail.jpg',
   fields: [buttonField(['default', 'secondary', 'ghost', 'outline'])],
 }
