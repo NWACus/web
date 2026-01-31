b2c0a4c322706ac4ce32e97d397217997b6389bb
rchlfryn <rachelafryan@gmail.com>
2026-01-30 16:22:40 -0800

    Update migration

--- src/migrations/20260128_012456_remove_wrap_in_container.ts
+++ src/migrations/20260128_012456_remove_wrap_in_container.ts
@@ -5,10 +5,16 @@ export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
     sql`ALTER TABLE \`home_pages_blocks_header_block\` ADD \`full_width_color\` integer DEFAULT true;`,
   )
   await db.run(
+    sql`UPDATE \`home_pages_blocks_header_block\` SET \`full_width_color\` = CASE WHEN \`wrap_in_container\` = 0 THEN 1 ELSE 0 END;`,
+  )
+  await db.run(
     sql`ALTER TABLE \`home_pages_blocks_header_block\` DROP COLUMN \`wrap_in_container\`;`,
   )
   await db.run(
     sql`ALTER TABLE \`_home_pages_v_blocks_header_block\` ADD \`full_width_color\` integer DEFAULT true;`,
+  )
+  await db.run(
+    sql`UPDATE \`_home_pages_v_blocks_header_block\` SET \`full_width_color\` = CASE WHEN \`wrap_in_container\` = 0 THEN 1 ELSE 0 END;`,
   )
   await db.run(
     sql`ALTER TABLE \`_home_pages_v_blocks_header_block\` DROP COLUMN \`wrap_in_container\`;`,
@@ -16,9 +22,15 @@ export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
   await db.run(
     sql`ALTER TABLE \`pages_blocks_header_block\` ADD \`full_width_color\` integer DEFAULT true;`,
   )
+  await db.run(
+    sql`UPDATE \`pages_blocks_header_block\` SET \`full_width_color\` = CASE WHEN \`wrap_in_container\` = 0 THEN 1 ELSE 0 END;`,
+  )
   await db.run(sql`ALTER TABLE \`pages_blocks_header_block\` DROP COLUMN \`wrap_in_container\`;`)
   await db.run(
     sql`ALTER TABLE \`_pages_v_blocks_header_block\` ADD \`full_width_color\` integer DEFAULT true;`,
+  )
+  await db.run(
+    sql`UPDATE \`_pages_v_blocks_header_block\` SET \`full_width_color\` = CASE WHEN \`wrap_in_container\` = 0 THEN 1 ELSE 0 END;`,
   )
   await db.run(sql`ALTER TABLE \`_pages_v_blocks_header_block\` DROP COLUMN \`wrap_in_container\`;`)
   await db.run(
@@ -51,6 +63,9 @@ export async function down({ db, payload, req }: MigrateDownArgs): Promise<void>
     sql`ALTER TABLE \`home_pages_blocks_header_block\` ADD \`wrap_in_container\` integer DEFAULT true;`,
   )
   await db.run(
+    sql`UPDATE \`home_pages_blocks_header_block\` SET \`wrap_in_container\` = CASE WHEN \`full_width_color\` = 0 THEN 1 ELSE 0 END;`,
+  )
+  await db.run(
     sql`ALTER TABLE \`home_pages_blocks_header_block\` DROP COLUMN \`full_width_color\`;`,
   )
   await db.run(
@@ -63,6 +78,9 @@ export async function down({ db, payload, req }: MigrateDownArgs): Promise<void>
     sql`ALTER TABLE \`_home_pages_v_blocks_header_block\` ADD \`wrap_in_container\` integer DEFAULT true;`,
   )
   await db.run(
+    sql`UPDATE \`_home_pages_v_blocks_header_block\` SET \`wrap_in_container\` = CASE WHEN \`full_width_color\` = 0 THEN 1 ELSE 0 END;`,
+  )
+  await db.run(
     sql`ALTER TABLE \`_home_pages_v_blocks_header_block\` DROP COLUMN \`full_width_color\`;`,
   )
   await db.run(
@@ -71,8 +89,12 @@ export async function down({ db, payload, req }: MigrateDownArgs): Promise<void>
   await db.run(
     sql`ALTER TABLE \`pages_blocks_document_block\` ADD \`wrap_in_container\` integer DEFAULT false;`,
   )
+
   await db.run(
     sql`ALTER TABLE \`pages_blocks_header_block\` ADD \`wrap_in_container\` integer DEFAULT true;`,
+  )
+  await db.run(
+    sql`UPDATE \`pages_blocks_header_block\` SET \`wrap_in_container\` = CASE WHEN \`full_width_color\` = 0 THEN 1 ELSE 0 END;`,
   )
   await db.run(sql`ALTER TABLE \`pages_blocks_header_block\` DROP COLUMN \`full_width_color\`;`)
   await db.run(
@@ -83,6 +105,9 @@ export async function down({ db, payload, req }: MigrateDownArgs): Promise<void>
   )
   await db.run(
     sql`ALTER TABLE \`_pages_v_blocks_header_block\` ADD \`wrap_in_container\` integer DEFAULT true;`,
+  )
+  await db.run(
+    sql`UPDATE \`_pages_v_blocks_header_block\` SET \`wrap_in_container\` = CASE WHEN \`full_width_color\` = 0 THEN 1 ELSE 0 END;`,
   )
   await db.run(sql`ALTER TABLE \`_pages_v_blocks_header_block\` DROP COLUMN \`full_width_color\`;`)
   await db.run(
