e02caab0525fe6593e5cc39b11d837bcc47653ba
rchlfryn <rachelafryan@gmail.com>
2026-02-01 11:52:17 -0800

    Update robots file for facebook bot

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,11 +5,25 @@ export default function robots(): MetadataRoute.Robots {
   const SITE_URL = getURL()
 
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: ['/admin', '/admin/'],
-    },
+    rules: [
+      {
+        userAgent: 'facebookexternaluser',
+        disallow: '/',
+      },
+      {
+        userAgent: 'facebookexternaluser',
+        disallow: '/',
+      },
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/admin', '/admin/'],
+      },
+      {
+        userAgent: '*',
+        disallow: ['/*.pdf', '/*.jpg', '/*.jpeg', '/*.png', '/*.gif', '/*.svg', '/*.webp'],
+      },
+    ],
     sitemap: `${SITE_URL}/sitemap.xml`,
   }
 }
