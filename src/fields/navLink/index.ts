67bfc4e9068d31c65120c7e1bb88e312dfa65376
Rachel Fryan <10836887+rchlfryn@users.noreply.github.com>
2026-02-07 13:18:23 -0800

    Revert "Undo nav link refactor"

--- src/fields/navLink/index.ts
+++ src/fields/navLink/index.ts
@@ -1,79 +1,8 @@
-import { clearIrrelevantLinkValues } from '@/utilities/clearIrrelevantLinkValues'
-import { getTenantFilter } from '@/utilities/collectionFilters'
-import { validateExternalUrl } from '@/utilities/validateUrl'
-import { NamedGroupField, TextFieldSingleValidation } from 'payload'
+import { linkField } from '@/fields/linkField'
 
-const validateLabel: TextFieldSingleValidation = (val, { siblingData }) => {
-  if (siblingData && typeof siblingData === 'object' && 'type' in siblingData) {
-    if (siblingData.type === 'internal') return true
-  }
-
-  return Boolean(val) || 'You must define a label for an external link.'
-}
-
-export const navLink: NamedGroupField = {
-  name: 'link',
-  type: 'group',
-  admin: {
-    hideGutter: true,
-  },
-  hooks: {
-    beforeChange: [clearIrrelevantLinkValues],
-  },
-  fields: [
-    {
-      name: 'type',
-      type: 'radio',
-      defaultValue: 'internal',
-      options: [
-        {
-          label: 'Internal link',
-          value: 'internal',
-        },
-        {
-          label: 'External link',
-          value: 'external',
-        },
-      ],
-    },
-    {
-      name: 'reference',
-      type: 'relationship',
-      admin: {
-        condition: (_, siblingData) => siblingData?.type === 'internal',
-      },
-      label: 'Select page or post',
-      relationTo: ['pages', 'builtInPages', 'posts'],
-      filterOptions: getTenantFilter,
-    },
-    {
-      name: 'url',
-      type: 'text',
-      admin: {
-        condition: (_, siblingData) => siblingData?.type === 'external',
-      },
-      label: 'External URL',
-      validate: validateExternalUrl,
-    },
-    {
-      name: 'label',
-      type: 'text',
-      admin: {
-        components: {
-          Description: '@/fields/navLink/components/LinkLabelDescription#LinkLabelDescription',
-        },
-      },
-      label: 'Label',
-      validate: validateLabel,
-    },
-    {
-      name: 'newTab',
-      type: 'checkbox',
-      defaultValue: true,
-      admin: {
-        condition: (_, siblingData) => siblingData?.type === 'external',
-      },
-      label: 'Open in new tab',
-    },
-  ],
-}
+export const navLink = linkField({
+  includeLabel: true,
+  newTabForExternalOnly: true,
+  labelDescriptionComponent:
+    '@/fields/navLink/components/LinkLabelDescription#LinkLabelDescription',
+})
