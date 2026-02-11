e1f956002b2eb0cd0cd62f0f4fb8a2082469b334
rchlfryn <rachelafryan@gmail.com>
2026-02-10 16:09:34 -0800

    Remove defer

--- src/blocks/GenericEmbed/Component.tsx
+++ src/blocks/GenericEmbed/Component.tsx
@@ -39,7 +39,6 @@ export const GenericEmbedBlockComponent = ({
         'allowpaymentrequest',
         'async',
         'campaign',
-        'defer',
         'enable-auto-scroll',
         'frameborder',
         'height',
