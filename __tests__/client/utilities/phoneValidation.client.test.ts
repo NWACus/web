40c16b8eeb2b18985575f1b56079d8f56d6f801f
rchlfryn <rachelafryan@gmail.com>
2026-02-16 09:04:01 -0800

    Move utility tests to their own folder

--- __tests__/client/utilities/phoneValidation.client.test.ts
+++ __tests__/client/utilities/phoneValidation.client.test.ts
@@ -2,7 +2,6 @@
 jest.mock('payload/shared', () => ({ text: jest.fn(() => true) }))
 
 import { phoneSchema } from '@/utilities/validatePhone'
-import { zipCodeSchema } from '@/utilities/validateZipCode'
 
 describe('phoneSchema', () => {
   it('accepts (123) 456-7890', () => {
@@ -43,35 +42,5 @@ describe('phoneSchema', () => {
 
   it('rejects empty string', () => {
     expect(() => phoneSchema.parse('')).toThrow()
-  })
-})
-
-describe('zipCodeSchema', () => {
-  it('accepts 5-digit ZIP code', () => {
-    expect(() => zipCodeSchema.parse('12345')).not.toThrow()
-  })
-
-  it('accepts 5+4 format ZIP code', () => {
-    expect(() => zipCodeSchema.parse('12345-6789')).not.toThrow()
-  })
-
-  it('rejects 4-digit code', () => {
-    expect(() => zipCodeSchema.parse('1234')).toThrow()
-  })
-
-  it('rejects 6-digit code', () => {
-    expect(() => zipCodeSchema.parse('123456')).toThrow()
-  })
-
-  it('rejects letters', () => {
-    expect(() => zipCodeSchema.parse('abcde')).toThrow()
-  })
-
-  it('rejects incomplete 5+4 format', () => {
-    expect(() => zipCodeSchema.parse('12345-67')).toThrow()
-  })
-
-  it('rejects empty string', () => {
-    expect(() => zipCodeSchema.parse('')).toThrow()
   })
 })
