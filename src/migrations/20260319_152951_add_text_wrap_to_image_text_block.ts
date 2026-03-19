a3cbc5341bf1ff06698d8c7bd0127038c733573e
rchlfryn <rachelafryan@gmail.com>
2026-03-19 10:31:11 -0500

    Fix migration from main merge

--- src/migrations/20260319_152951_add_text_wrap_to_image_text_block.ts
+++ src/migrations/20260319_152951_add_text_wrap_to_image_text_block.ts
@@ -1,10 +1,18 @@
-import { MigrateUpArgs, MigrateDownArgs, sql } from '@payloadcms/db-sqlite'
+import { MigrateDownArgs, MigrateUpArgs, sql } from '@payloadcms/db-sqlite'
 
 export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
-  await db.run(sql`ALTER TABLE \`home_pages_blocks_image_text\` ADD \`text_wrap\` integer DEFAULT false;`)
-  await db.run(sql`ALTER TABLE \`_home_pages_v_blocks_image_text\` ADD \`text_wrap\` integer DEFAULT false;`)
-  await db.run(sql`ALTER TABLE \`pages_blocks_image_text\` ADD \`text_wrap\` integer DEFAULT false;`)
-  await db.run(sql`ALTER TABLE \`_pages_v_blocks_image_text\` ADD \`text_wrap\` integer DEFAULT false;`)
+  await db.run(
+    sql`ALTER TABLE \`home_pages_blocks_image_text\` ADD \`text_wrap\` integer DEFAULT false;`,
+  )
+  await db.run(
+    sql`ALTER TABLE \`_home_pages_v_blocks_image_text\` ADD \`text_wrap\` integer DEFAULT false;`,
+  )
+  await db.run(
+    sql`ALTER TABLE \`pages_blocks_image_text\` ADD \`text_wrap\` integer DEFAULT false;`,
+  )
+  await db.run(
+    sql`ALTER TABLE \`_pages_v_blocks_image_text\` ADD \`text_wrap\` integer DEFAULT false;`,
+  )
 }
 
 export async function down({ db, payload, req }: MigrateDownArgs): Promise<void> {
