d390444874f42172ba3767982145639f2aecaeb1
rchlfryn <rachelafryan@gmail.com>
2026-03-20 08:16:44 -0500

    Use custom slug field to only show unused tenants

---
+++ src/collections/Tenants/components/TenantSlugField.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+import type { SelectFieldClientProps } from 'payload'
+
+import { SelectField, useDocumentInfo } from '@payloadcms/ui'
+import { useEffect, useState } from 'react'
+
+export const TenantSlugField = (props: SelectFieldClientProps) => {
+  const { id } = useDocumentInfo()
+  const [usedSlugs, setUsedSlugs] = useState<string[]>([])
+
+  useEffect(() => {
+    if (id) return
+
+    fetch('/api/tenants?limit=0&depth=0&select[slug]=true')
+      .then((res) => res.json())
+      .then((data) => setUsedSlugs(data.docs.map((doc: { slug: string }) => doc.slug)))
+      .catch(() => {})
+  }, [id])
+
+  const options = id
+    ? props.field.options
+    : props.field.options?.filter((option) => {
+        const value = typeof option === 'string' ? option : option.value
+        return !usedSlugs.includes(value)
+      })
+
+  return <SelectField {...props} field={{ ...props.field, options }} />
+}
