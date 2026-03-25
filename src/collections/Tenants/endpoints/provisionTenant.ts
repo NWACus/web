88cd442bedfffb59f48c76c5a88e579f1faf8893
rchlfryn <rachelafryan@gmail.com>
2026-03-25 16:13:51 -0500

    Fix navigation failing to provision

--- src/collections/Tenants/endpoints/provisionTenant.ts
+++ src/collections/Tenants/endpoints/provisionTenant.ts
@@ -596,6 +596,14 @@ export async function provision(payload: Payload, tenant: Tenant) {
               navPageItem('avalanche-accident-map'),
             ]),
           },
+          blog: {
+            link: navBuiltInPageItem('/blog', 'Blog')?.link,
+            options: { enabled: true },
+          },
+          events: {
+            link: navBuiltInPageItem('/events', 'Events')?.link,
+            options: { enabled: true },
+          },
           donate: {
             options: { displayMode: 'button' },
             link: navPageItem('donate-membership', 'Donate')?.link,
