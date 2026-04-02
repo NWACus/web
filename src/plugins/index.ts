19c7f33cb93a1890eb482e4748dd28e1c73ae6f0
rchlfryn <rachelafryan@gmail.com>
2026-04-02 15:01:41 -0700

    Update payload packages to 3.81.0

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -77,6 +77,7 @@ export const plugins: Plugin[] = [
       },
     },
     clientUploads: true,
+    allowOverwrite: true,
     token: process.env.VERCEL_BLOB_READ_WRITE_TOKEN,
   }),
   sentryPlugin({
