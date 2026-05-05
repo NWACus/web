53fd26812182d50180760a6acce7b9ce582821d6
Kellen Busby <kellenbusby@gmail.com>
2026-05-04 17:08:44 -0700

    combining migrations

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -47,12 +47,8 @@ import * as migration_20260131_012456_remove_wrap_in_container from './20260131_
 import * as migration_20260303_233752_remove_custom_domain_from_tenants from './20260303_233752_remove_custom_domain_from_tenants'
 import * as migration_20260319_152951_add_text_wrap_to_image_text_block from './20260319_152951_add_text_wrap_to_image_text_block'
 import * as migration_20260403_204010 from './20260403_204010'
-import * as migration_20260414_030626_add_documentReferences_field from './20260414_030626_add_documentReferences_field'
-import * as migration_20260414_030934_backfill_document_references from './20260414_030934_backfill_document_references'
-import * as migration_20260417_202409_add_dev_mode from './20260417_202409_add_dev_mode'
-import * as migration_20260417_225756_remove_old_block_tracking_fields from './20260417_225756_remove_old_block_tracking_fields'
-import * as migration_20260424_180242_intermediate_collection_document_references from './20260424_180242_intermediate_collection_document_references'
-import * as migration_20260424_180419_backfill_intermediate_document_references from './20260424_180419_backfill_intermediate_document_references'
+import * as migration_20260504_235946_revalidation_fields_and_widgets_dev_mode from './20260504_235946_revalidation_fields_and_widgets_dev_mode'
+import * as migration_20260505_000519_backfill_document_references from './20260505_000519_backfill_document_references'
 
 export const migrations = [
   {
@@ -301,33 +297,13 @@ export const migrations = [
     name: '20260403_204010',
   },
   {
-    up: migration_20260414_030626_add_documentReferences_field.up,
-    down: migration_20260414_030626_add_documentReferences_field.down,
-    name: '20260414_030626_add_documentReferences_field',
+    up: migration_20260504_235946_revalidation_fields_and_widgets_dev_mode.up,
+    down: migration_20260504_235946_revalidation_fields_and_widgets_dev_mode.down,
+    name: '20260504_235946_revalidation_fields_and_widgets_dev_mode',
   },
   {
-    up: migration_20260414_030934_backfill_document_references.up,
-    down: migration_20260414_030934_backfill_document_references.down,
-    name: '20260414_030934_backfill_document_references',
-  },
-  {
-    up: migration_20260417_202409_add_dev_mode.up,
-    down: migration_20260417_202409_add_dev_mode.down,
-    name: '20260417_202409_add_dev_mode',
-  },
-  {
-    up: migration_20260417_225756_remove_old_block_tracking_fields.up,
-    down: migration_20260417_225756_remove_old_block_tracking_fields.down,
-    name: '20260417_225756_remove_old_block_tracking_fields',
-  },
-  {
-    up: migration_20260424_180242_intermediate_collection_document_references.up,
-    down: migration_20260424_180242_intermediate_collection_document_references.down,
-    name: '20260424_180242_intermediate_collection_document_references',
-  },
-  {
-    up: migration_20260424_180419_backfill_intermediate_document_references.up,
-    down: migration_20260424_180419_backfill_intermediate_document_references.down,
-    name: '20260424_180419_backfill_intermediate_document_references',
+    up: migration_20260505_000519_backfill_document_references.up,
+    down: migration_20260505_000519_backfill_document_references.down,
+    name: '20260505_000519_backfill_document_references',
   },
 ]
